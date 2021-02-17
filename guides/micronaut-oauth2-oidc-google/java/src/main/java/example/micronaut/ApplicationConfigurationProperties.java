da4b6abbfd0e6f514526891182fad1c0097e902a
Sergio del Amo <sergio.delamo@softamo.com>
2021-02-17 10:00:54 +0100

    guide: Secure a Micronaut app with Google (#114)
    * guide: Secure a Micronaut app with Google
    
    Close: https://github.com/micronaut-projects/micronaut-guides-poc/issues/70
    
    * Fix missing callouts and minor changes in guide
    
    Co-authored-by: Iván López <lopezi@objectcomputing.com>

---
+++ guides/micronaut-oauth2-oidc-google/java/src/main/java/example/micronaut/ApplicationConfigurationProperties.java
@@ -0,0 +1,23 @@
+package example.micronaut;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.context.annotation.Requires;
+
+@Requires(property = "app.hosted-domain")
+@ConfigurationProperties("app")
+public class ApplicationConfigurationProperties implements ApplicationConfiguration {
+
+    @NonNull
+    private String hostedDomain;
+
+    public void setHostedDomain(@NonNull String hostedDomain) {
+        this.hostedDomain = hostedDomain;
+    }
+
+    @Override
+    @NonNull
+    public String getHostedDomain() {
+        return hostedDomain;
+    }
+}
