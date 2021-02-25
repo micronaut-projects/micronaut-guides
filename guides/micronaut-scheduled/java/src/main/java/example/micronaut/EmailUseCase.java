6b2418b8f6e13b6ccda2e976c68bd395a3ccf49f
Iván López <lopezi@objectcomputing.com>
2021-02-25 12:43:22 +0100

    Migrate Scheduled guide
    Closes #17

--- guides/micronaut-scheduled/java/src/main/java/example/micronaut/EmailUseCase.java
+++ guides/micronaut-scheduled/java/src/main/java/example/micronaut/EmailUseCase.java
@@ -9,7 +9,7 @@ import java.util.Date;
 
 @Singleton
 public class EmailUseCase {
-    private static final Logger LOG = LoggerFactory.getLogger(EmailUseCase.class); // <2>
+    private static final Logger LOG = LoggerFactory.getLogger(EmailUseCase.class);
 
     void send(String user, String message) {
         LOG.info("Sending email to {}: {} at {}", user, message, new SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(new Date()));
