c0a151a5213a0b232da63ccf732d4f75ea05588c
Iván López <lopezi@objectcomputing.com>
2021-03-10 11:20:20 +0100

    Add Geb feature and use it in error-handling guide (#142)

---
+++ buildSrc/src/main/java/io/micronaut/guides/feature/GebFeature.java
@@ -0,0 +1,35 @@
+package io.micronaut.guides.feature;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import io.micronaut.starter.application.ApplicationType;
+import io.micronaut.starter.application.generator.GeneratorContext;
+import io.micronaut.starter.build.dependencies.Dependency;
+import io.micronaut.starter.feature.Feature;
+import io.micronaut.starter.options.TestFramework;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class GebFeature implements Feature {
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "geb";
+    }
+
+    @Override
+    public boolean supports(ApplicationType applicationType) {
+        return true;
+    }
+
+    @Override
+    public void apply(GeneratorContext generatorContext) {
+        if (generatorContext.getTestFramework().equals(TestFramework.JUNIT)) {
+            generatorContext.addDependency(Dependency.builder().groupId("org.gebish").lookupArtifactId("geb-junit").test());
+        } else if (generatorContext.getTestFramework().equals(TestFramework.SPOCK)) {
+            generatorContext.addDependency(Dependency.builder().groupId("org.gebish").lookupArtifactId("geb-spock").test());
+        }
+        generatorContext.addDependency(Dependency.builder().groupId("org.seleniumhq.selenium").lookupArtifactId("htmlunit-driver").test());
+    }
+}
