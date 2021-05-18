d72d77c89bf08af325163f4d6bb47d1f3171c517
Sergio del Amo <sergio.delamo@softamo.com>
2021-05-18 10:21:43 +0200

    www-authenticate section
    see: https://github.com/micronaut-projects/micronaut-security/issues/648

---
+++ guides/micronaut-security-basicauth/java/src/main/java/example/micronaut/DefaultAuthorizationExceptionHandlerReplacement.java
@@ -0,0 +1,25 @@
+package example.micronaut;
+
+import io.micronaut.context.annotation.Replaces;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.MutableHttpResponse;
+import io.micronaut.security.authentication.AuthorizationException;
+import io.micronaut.security.authentication.DefaultAuthorizationExceptionHandler;
+
+import javax.inject.Singleton;
+
+@Singleton // <1>
+@Replaces(DefaultAuthorizationExceptionHandler.class) // <2>
+public class DefaultAuthorizationExceptionHandlerReplacement extends DefaultAuthorizationExceptionHandler {
+
+    @Override
+    protected MutableHttpResponse<?> httpResponseWithStatus(HttpRequest request, AuthorizationException exception) {
+        if (exception.isForbidden()) {
+            return HttpResponse.status(HttpStatus.FORBIDDEN);
+        }
+        return HttpResponse.status(HttpStatus.UNAUTHORIZED)
+                .header("WWW-Authenticate", "Basic realm=\"Micronaut Guide\"");
+    }
+}
