acadd55e55820c4a4c128579cdc4c5d910b7affe
Sergio del Amo <sergio.delamo@softamo.com>
2021-05-18 15:01:56 +0200

    www-authenticate section (#246)
    see: https://github.com/micronaut-projects/micronaut-security/issues/648

---
+++ guides/micronaut-security-basicauth/kotlin/src/main/kotlin/example/micronaut/DefaultAuthorizationExceptionHandlerReplacement.kt
@@ -0,0 +1,24 @@
+package example.micronaut
+
+import io.micronaut.context.annotation.Replaces
+import io.micronaut.security.authentication.AuthorizationException
+import io.micronaut.security.authentication.DefaultAuthorizationExceptionHandler
+import javax.inject.Singleton
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.MutableHttpResponse
+
+@Singleton // <1>
+@Replaces(DefaultAuthorizationExceptionHandler::class)  // <2>
+class DefaultAuthorizationExceptionHandlerReplacement : DefaultAuthorizationExceptionHandler() {
+    override fun httpResponseWithStatus(
+        request: HttpRequest<*>?,
+        exception: AuthorizationException?
+    ): MutableHttpResponse<*> {
+        return if (exception?.isForbidden == true) {
+            HttpResponse.status<Any>(HttpStatus.FORBIDDEN);
+        } else HttpResponse.status<Any>(HttpStatus.UNAUTHORIZED)
+            .header("WWW-Authenticate", "Basic realm=\"Micronaut Guide\"");
+    }
+}
