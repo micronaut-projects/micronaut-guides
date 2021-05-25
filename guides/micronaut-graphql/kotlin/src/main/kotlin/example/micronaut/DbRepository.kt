92e7af64aa3ba1709abb7c79fe73fb9ea334c48b
Iván López <lopezi@objectcomputing.com>
2021-05-25 10:20:41 +0200

    Add GraphQL guide

---
+++ guides/micronaut-graphql/kotlin/src/main/kotlin/example/micronaut/DbRepository.kt
@@ -0,0 +1,26 @@
+package example.micronaut
+
+import java.util.stream.Collectors
+import javax.inject.Singleton
+
+@Singleton
+class DbRepository {
+
+    fun findAllBooks(): List<Book> {
+        return books
+    }
+
+    fun findAllAuthors(): List<Author> {
+        return books.stream()
+            .map(Book::author)
+            .collect(Collectors.toList())
+    }
+
+    companion object {
+        private val books = listOf( // <1>
+            Book("book-1", "Harry Potter and the Philosopher's Stone", 223, Author("author-1", "Joanne", "Rowling")),
+            Book("book-2", "Moby Dick", 635, Author("author-2", "Herman", "Melville")),
+            Book("book-3", "Interview with the vampire", 371, Author("author-3", "Anne", "Rice"))
+        )
+    }
+}
---
+++ guides/micronaut-graphql/kotlin/src/test/kotlin/example/micronaut/GraphQLControllerTest.kt
@@ -0,0 +1,39 @@
+package example.micronaut
+
+import io.micronaut.core.type.Argument
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import javax.inject.Inject
+
+@MicronautTest
+class GraphQLControllerTest {
+
+    @Inject
+    @field:Client("/")
+    lateinit var client: RxHttpClient // <2>
+
+    @Test
+    fun testGraphQLController() {
+        val query = "{ \"query\": \"{ bookById(id:\\\"book-1\\\") { name, pageCount, author { firstName, lastName} } }\" }"
+        val request: HttpRequest<String> = HttpRequest.POST("/graphql", query)
+
+        val rsp = client.toBlocking().exchange(request, Argument.of(Map::class.java))
+        assertEquals(HttpStatus.OK, rsp.status())
+        Assertions.assertNotNull(rsp.body())
+
+        val bookInfo = rsp.getBody(Map::class.java).get().get("data") as Map<*, *>
+        val bookById = bookInfo["bookById"] as Map<*, *>?
+        assertEquals("Harry Potter and the Philosopher's Stone", bookById!!["name"])
+        assertEquals(223, bookById["pageCount"])
+
+        val author = bookById["author"] as Map<*, *>?
+        assertEquals("Joanne", author!!["firstName"])
+        assertEquals("Rowling", author["lastName"])
+    }
+}
