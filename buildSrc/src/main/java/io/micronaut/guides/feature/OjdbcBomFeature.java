ef8a2fc78870043936ce98c5b27e0937e8065e96
Burt Beckwith <burt@burtbeckwith.com>
2021-05-28 10:17:41 -0400

    new Oracle driver bom and Oracle Cloud ATP features

---
+++ buildSrc/src/main/java/io/micronaut/guides/feature/OjdbcBomFeature.java
@@ -0,0 +1,35 @@
+package io.micronaut.guides.feature;
+
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.starter.application.ApplicationType;
+import io.micronaut.starter.application.generator.GeneratorContext;
+import io.micronaut.starter.build.dependencies.Dependency;
+import io.micronaut.starter.build.dependencies.Scope;
+import io.micronaut.starter.feature.Feature;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class OjdbcBomFeature implements Feature {
+
+    @Override
+    public void apply(GeneratorContext generatorContext) {
+        generatorContext.addDependency(Dependency.builder()
+                .groupId("com.oracle.database.jdbc")
+                .lookupArtifactId("ojdbc-bom")
+                .pom(true)
+                .scope(Scope.COMPILE)
+                .build());
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "ojdbc-bom";
+    }
+
+    @Override
+    public boolean supports(ApplicationType applicationType) {
+        return true;
+    }
+}
