e40c26284606dd96e71445fdae3297c25a309731
Sergio del Amo <sergio.delamo@softamo.com>
2021-06-09 09:23:24 +0200

    add jwks guide (#282)
    Co-authored-by: Iván López <lopezi@objectcomputing.com>

---
+++ guides/micronaut-security-keys-jwks/java/src/main/java/example/micronaut/JwkConfigurationProperties.java
@@ -0,0 +1,38 @@
+package example.micronaut;
+
+import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.core.annotation.NonNull;
+
+import javax.validation.constraints.NotBlank;
+
+@ConfigurationProperties("jwk") // <1>
+public class JwkConfigurationProperties implements JwkConfiguration {
+
+    @NonNull
+    @NotBlank // <2>
+    private String primary;
+
+    @NonNull
+    @NotBlank // <2>
+    private String secondary;
+
+    @Override
+    @NonNull
+    public String getPrimary() {
+        return primary;
+    }
+
+    @Override
+    @NonNull
+    public String getSecondary() {
+        return secondary;
+    }
+
+    public void setPrimary(@NonNull String primary) {
+        this.primary = primary;
+    }
+
+    public void setSecondary(@NonNull String secondary) {
+        this.secondary = secondary;
+    }
+}
