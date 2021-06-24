b3cea23d6a0f4b65623136572cb5afdeb8958bf4
Burt Beckwith <burt@burtbeckwith.com>
2021-06-24 00:19:53 -0400

    simplify features by moving common logic to a base class

--- buildSrc/src/main/java/io/micronaut/guides/feature/NimbusJoseJwt.java
+++ buildSrc/src/main/java/io/micronaut/guides/feature/NimbusJoseJwt.java
@@ -1,31 +1,11 @@
 package io.micronaut.guides.feature;
 
-import io.micronaut.core.annotation.NonNull;
-import io.micronaut.starter.application.ApplicationType;
-import io.micronaut.starter.application.generator.GeneratorContext;
-import io.micronaut.starter.build.dependencies.Dependency;
-import io.micronaut.starter.feature.Feature;
 import javax.inject.Singleton;
 
 @Singleton
-public class NimbusJoseJwt implements Feature {
+public class NimbusJoseJwt extends AbstractFeature {
 
-    @NonNull
-    @Override
-    public String getName() {
-        return "nimbus-jose-jwt";
-    }
-
-    @Override
-    public boolean supports(ApplicationType applicationType) {
-        return true;
-    }
-
-    @Override
-    public void apply(GeneratorContext generatorContext) {
-        generatorContext.addDependency(Dependency.builder()
-                .lookupArtifactId("nimbus-jose-jwt")
-                .compile()
-                .build());
+    public NimbusJoseJwt() {
+        super("nimbus-jose-jwt");
     }
 }
