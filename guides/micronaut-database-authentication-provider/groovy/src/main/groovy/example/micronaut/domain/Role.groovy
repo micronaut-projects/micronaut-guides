b3c3ea5bdf83781ba08d9bedb867e4c9f43889e5
Sergio del Amo <sergio.delamo@softamo.com>
2021-07-13 17:23:01 +0200

    Annotated gorm entities as introspected (#369)
    * Revert "ignore gorm tests (#360)"
    
    This reverts commit 6501790856e9c9f07d9dbdc3bbf40f4c53cea9e5.
    
    * annotate Gorm entities with @Introspected
    
    Close: https://github.com/micronaut-projects/micronaut-guides/issues/366

--- guides/micronaut-database-authentication-provider/groovy/src/main/groovy/example/micronaut/domain/Role.groovy
+++ guides/micronaut-database-authentication-provider/groovy/src/main/groovy/example/micronaut/domain/Role.groovy
@@ -2,7 +2,9 @@ package example.micronaut.domain
 
 import grails.gorm.annotation.Entity
 import org.grails.datastore.gorm.GormEntity
+import io.micronaut.core.annotation.Introspected
 
+@Introspected
 @Entity // <1>
 class Role implements GormEntity<Role> {  // <2>
     String authority
