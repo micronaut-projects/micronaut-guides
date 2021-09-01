c7d927fc5cdac27169c79774a2ea9de5c15657c5
Burt Beckwith <burt@burtbeckwith.com>
2021-09-01 09:43:47 -0400

    micronaut-security-basicauth fixes (#460)
    * minor code fixes/cleanup
    
    * add missing reactor and graalvm features, add features to app creation instructions
    
    * PR feedback

--- guides/micronaut-security-basicauth/java/src/test/java/example/micronaut/BasicAuthClientTest.java
+++ guides/micronaut-security-basicauth/java/src/test/java/example/micronaut/BasicAuthClientTest.java
@@ -1,9 +1,9 @@
 package example.micronaut;
 
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
-import jakarta.inject.Inject;
 import java.util.Base64;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,8 +17,7 @@ public class BasicAuthClientTest {
     @Test
     void verifyBasicAuthWorks() {
         String credsEncoded = Base64.getEncoder().encodeToString("sherlock:password".getBytes());
-        String rsp = appClient.home("Basic " +credsEncoded); // <3>
-        assertEquals(rsp, "sherlock");
+        String rsp = appClient.home("Basic " + credsEncoded); // <3>
+        assertEquals("sherlock", rsp);
     }
-
 }
