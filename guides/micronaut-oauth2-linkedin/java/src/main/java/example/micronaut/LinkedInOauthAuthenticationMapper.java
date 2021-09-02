a086dc2677cbba567d27d31904a7a50ed6c2d62f
Burt Beckwith <burt@burtbeckwith.com>
2021-09-02 07:04:50 -0400

    Kotlin and Groovy versions of code for micronaut-oauth2-linkedin guide (#477)
    * make POJO immutable
    
    * imports
    
    * add Kotlin and Java source
    
    * PR feedback

--- guides/micronaut-oauth2-linkedin/java/src/main/java/example/micronaut/LinkedInOauthAuthenticationMapper.java
+++ guides/micronaut-oauth2-linkedin/java/src/main/java/example/micronaut/LinkedInOauthAuthenticationMapper.java
@@ -1,23 +1,20 @@
 package example.micronaut;
 
 import io.micronaut.core.annotation.Nullable;
-import io.micronaut.core.async.publisher.Publishers;
 import io.micronaut.core.util.CollectionUtils;
-import io.micronaut.http.HttpHeaderValues;
-import io.micronaut.security.authentication.AuthenticationResponse;
-import io.micronaut.security.authentication.AuthenticationFailed;
-import io.micronaut.security.authentication.AuthenticationFailureReason;
 import io.micronaut.security.authentication.AuthenticationResponse;
 import io.micronaut.security.oauth2.endpoint.authorization.state.State;
 import io.micronaut.security.oauth2.endpoint.token.response.OauthAuthenticationMapper;
 import io.micronaut.security.oauth2.endpoint.token.response.TokenResponse;
-import reactor.core.publisher.Mono;
-import org.reactivestreams.Publisher;
-
 import jakarta.inject.Named;
 import jakarta.inject.Singleton;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Mono;
+
 import java.util.Collections;
 import java.util.Map;
+
+import static io.micronaut.http.HttpHeaderValues.AUTHORIZATION_PREFIX_BEARER;
 
 @Named("linkedin")
 @Singleton
@@ -32,9 +29,10 @@ public class LinkedInOauthAuthenticationMapper implements OauthAuthenticationMap
     @Override
     public Publisher<AuthenticationResponse> createAuthenticationResponse(TokenResponse tokenResponse,
                                                                           @Nullable State state) {
-        return Mono.from(linkedInApiClient.me(HttpHeaderValues.AUTHORIZATION_PREFIX_BEARER + " " + tokenResponse.getAccessToken()))
+        return Mono.from(linkedInApiClient.me(AUTHORIZATION_PREFIX_BEARER + ' ' + tokenResponse.getAccessToken()))
                 .map(linkedMe -> {
-                    Map<String, Object> attributes = CollectionUtils.mapOf("firstName", linkedMe.getLocalizedFirstName(),
+                    Map<String, Object> attributes = CollectionUtils.mapOf(
+                            "firstName", linkedMe.getLocalizedFirstName(),
                             "lastName", linkedMe.getLocalizedLastName());
                     String username = linkedMe.getId();
                     return AuthenticationResponse.success(username, Collections.emptyList(), attributes);
