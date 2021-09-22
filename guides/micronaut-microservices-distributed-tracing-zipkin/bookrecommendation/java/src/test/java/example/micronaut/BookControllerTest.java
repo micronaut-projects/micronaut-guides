9eb35a747e6219b5fabbbd36159255392a0f8600
Sergio del Amo <sergio.delamo@softamo.com>
2021-09-22 11:46:59 +0200

    remove StreamingHttpClient mentions (#543)

--- guides/micronaut-microservices-distributed-tracing-zipkin/bookrecommendation/java/src/test/java/example/micronaut/BookControllerTest.java
+++ guides/micronaut-microservices-distributed-tracing-zipkin/bookrecommendation/java/src/test/java/example/micronaut/BookControllerTest.java
@@ -1,29 +1,32 @@
 package example.micronaut;
 
+import io.micronaut.core.type.Argument;
 import io.micronaut.http.HttpRequest;
-import io.micronaut.http.client.StreamingHttpClient;
+import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import reactor.core.publisher.Flux;
-import org.junit.jupiter.api.Test;
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 public class BookControllerTest {
 
     @Inject
     @Client("/")
-    StreamingHttpClient client;
+    HttpClient client;
 
     @DisabledIfEnvironmentVariable(named = "CI", matches = "true")
     @Test
     public void testRetrieveBooks() {
-        List<BookRecommendation> books = Flux.from(client.jsonStream(HttpRequest.GET("/books"), BookRecommendation.class)).collectList().block();
-        assertEquals(books.size(), 1);
-        assertEquals(books.get(0).getName(), "Building Microservices");
+        List<BookRecommendation> books = client.toBlocking()
+                .retrieve(HttpRequest.GET("/books"), Argument.listOf(BookRecommendation.class));
+        assertEquals(1, books.size());
+        assertEquals("Building Microservices", books.get(0).getName());
     }
 }
