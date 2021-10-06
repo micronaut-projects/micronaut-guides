0f5e04bfbcd9658decd1453a3910b274beb63d57
Iván López <lopezi@objectcomputing.com>
2021-10-06 04:22:01 +0200

    Explain `GithubFilterCondition` for HttpClient Kotlin guide (#564)
    Fixes #463

--- guides/micronaut-http-client/kotlin/src/main/kotlin/example/micronaut/GithubFilterCondition.kt
+++ guides/micronaut-http-client/kotlin/src/main/kotlin/example/micronaut/GithubFilterCondition.kt
@@ -4,15 +4,15 @@ import io.micronaut.context.condition.Condition
 import io.micronaut.context.condition.ConditionContext
 import io.micronaut.context.exceptions.NoSuchBeanException
 
-class GithubFilterCondition : Condition {
+class GithubFilterCondition : Condition { // <1>
 
     override fun matches(context: ConditionContext<*>?): Boolean {
         if (context != null && context.beanContext != null) {
             try {
                 val githubConfiguration: GithubConfiguration =
-                    context.beanContext.getBean(GithubConfiguration::class.java)
+                    context.beanContext.getBean(GithubConfiguration::class.java) // <2>
                 if (githubConfiguration.token != null && githubConfiguration.username != null) {
-                    return true
+                    return true // <3>
                 }
             } catch (e: NoSuchBeanException) {
             }
