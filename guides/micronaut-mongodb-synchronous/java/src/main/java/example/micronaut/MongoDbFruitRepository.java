3c7286aff51f743eedd6973baf06c417da749303
Sergio del Amo <sergio.delamo@softamo.com>
2021-10-13 20:14:41 +0200

    Mongo sync guide (#528)
    * Mongo sync guide
    
    * add twitter card
    
    * don't include test
    
    * extract MonoDbUtils return Iterable, FruitsClient
    
    * remove extra quotes
    
    * remove 's
    
    * move @ExecuteOn to the class level
    
    * use @BsonCreator and @BsonProperty
    
    * use iterable into
    
    * simplify test
    
    * Proofreading (#534)
    
    Just a couple of minor changes.
    
    * update connection sentence
    
    Co-authored-by: graemerocher <graeme.rocher@gmail.com>
    Co-authored-by: Kristin Kroeger <33693760+kroegerk@users.noreply.github.com>

---
+++ guides/micronaut-mongodb-synchronous/java/src/main/java/example/micronaut/MongoDbFruitRepository.java
@@ -0,0 +1,40 @@
+package example.micronaut;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import io.micronaut.core.annotation.NonNull;
+import jakarta.inject.Singleton;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Singleton // <1>
+public class MongoDbFruitRepository implements FruitRepository {
+
+    private final MongoDbConfiguration mongoConf;
+    private final MongoClient mongoClient;
+
+    public MongoDbFruitRepository(MongoDbConfiguration mongoConf,  // <2>
+                                  MongoClient mongoClient) {  // <3>
+        this.mongoConf = mongoConf;
+        this.mongoClient = mongoClient;
+    }
+
+    @Override
+    public void save(@NonNull @NotNull @Valid Fruit fruit){
+        getCollection().insertOne(fruit);
+    }
+
+    @Override
+    @NonNull
+    public List<Fruit> list() {
+        return getCollection().find().into(new ArrayList<>());
+    }
+    
+    @NonNull
+    private MongoCollection<Fruit> getCollection(){
+        return mongoClient.getDatabase(mongoConf.getName())
+                .getCollection(mongoConf.getCollection(), Fruit.class);
+    }
+}
