d811cadeabb086628abf0a52f13e6858cf31d770
Sergio del Amo <sergio.delamo@softamo.com>
2021-10-14 11:17:07 +0200

    simplify guide using additional-request-params

--- guides/micronaut-oauth2-client-credentials-auth0/bookrecommendation/kotlin/src/main/kotlin/example/micronaut/Auth0Configuration.kt
+++
@@ -1,8 +0,0 @@
-package example.micronaut
-
-import io.micronaut.context.annotation.ConfigurationProperties
-
-@ConfigurationProperties("auth0") // <1>
-interface Auth0Configuration {
-    fun getApiIdentifier(): String
-}
