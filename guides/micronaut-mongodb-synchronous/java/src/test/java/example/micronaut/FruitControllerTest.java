c49f511066bd979485f4565b6b2119b6e9404304
Sergio del Amo <sergio.delamo@softamo.com>
2021-10-20 12:04:25 +0200

    test: more idiomatic mongodb tests (#590)
    Close https://github.com/micronaut-projects/micronaut-guides/issues/576

--- guides/micronaut-mongodb-synchronous/java/src/test/java/example/micronaut/FruitControllerTest.java
+++ guides/micronaut-mongodb-synchronous/java/src/test/java/example/micronaut/FruitControllerTest.java
@@ -3,28 +3,21 @@ package example.micronaut;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import io.micronaut.test.support.TestPropertyProvider;
-import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.TestInstance;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.utility.DockerImageName;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 @MicronautTest
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestInstance(Lifecycle.PER_CLASS)
 public class FruitControllerTest implements TestPropertyProvider {
-    static MongoDBContainer mongoDBContainer;
-    @Inject FruitClient fruitClient;
 
     @Test
-    void fruitsEndpointInteractsWithMongo() {        
+    void fruitsEndpointInteractsWithMongo(FruitClient fruitClient) {
         List<Fruit> fruits = fruitClient.findAll();
         assertTrue(fruits.isEmpty());
 
@@ -40,24 +33,19 @@ public class FruitControllerTest implements TestPropertyProvider {
         assertEquals(HttpStatus.CREATED, status);
         fruits = fruitClient.findAll();
         assertTrue(fruits.stream()
-            .filter(f -> f.getDescription() != null && f.getDescription().equals("Keeps the doctor away"))
-            .findFirst()
-            .isPresent());
+                .filter(f -> f.getDescription() != null && f.getDescription().equals("Keeps the doctor away"))
+                .findFirst()
+                .isPresent());
+    }
+
+    @AfterAll
+    static void cleanup() {
+        MongoDbUtils.closeMongoDb();
     }
 
     @Override
     public Map<String, String> getProperties() {
-        mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"))
-                                .withExposedPorts(27017);
-
-        mongoDBContainer.start();
-        return Collections.singletonMap("mongodb.uri", mongoDBContainer.getReplicaSetUrl());
-    }
-
-    @AfterAll
-    static void shutdownMongo() {
-        if (mongoDBContainer != null) {
-            mongoDBContainer.stop();
-        }
+        MongoDbUtils.startMongoDb();
+        return Collections.singletonMap("mongodb.uri", MongoDbUtils.getMongoDbUri());
     }
 }
