61356af9105f737962a314b6ce3d1f248a5c3737
Sergio del Amo <sergio.delamo@softamo.com>
2021-10-21 11:00:32 +0200

    add missing produces import

--- guides/micronaut-google-app-engine/kotlin/src/main/kotlin/example/micronaut/HelloController.kt
+++ guides/micronaut-google-app-engine/kotlin/src/main/kotlin/example/micronaut/HelloController.kt
@@ -3,6 +3,7 @@ package example.micronaut
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Produces
 
 @Controller("/hello") // <1>
  class HelloController {
