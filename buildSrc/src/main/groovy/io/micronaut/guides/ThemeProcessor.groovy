c665ac360b948749026f81b515711d0c2bf3e27c
Sergio del Amo <sergio.delamo@softamo.com>
2022-02-06 18:45:27 +0100

    design: increase width of guides starting at fourth section (#749)

--- buildSrc/src/main/groovy/io/micronaut/guides/ThemeProcessor.groovy
+++ buildSrc/src/main/groovy/io/micronaut/guides/ThemeProcessor.groovy
@@ -6,6 +6,9 @@ class ThemeProcessor {
 
     private static final Pattern TOC_REGEX = ~/(?s)<div id="toc" class="toc2">\s*<div id="toctitle">Table of Contents<\/div>(.*)<\/div>\s*<\/div>\s*<div id="content">/
     private static final Pattern CONTENT_REGEX = ~/(?s)<div class="sectionbody">(.*)<\/div>\s*<\/div>\s*<\/div>\s*<\/body>/
+    private static final String HTML_SECT1 = "<div class=\"sect1\">";
+    private static final String HTML_DIV_COL_MD_8 = "</div></div><div class=\"row\"><div class=\"col-md-12\">";
+
 
     static void applyThemes(File template, File dist,
                             File guidesFolder, String metadataConfigName) {
@@ -46,10 +49,34 @@ class ThemeProcessor {
                     text = text.replace("@bodyclass@", 'guide')
                     text = text.replace("@toccontent@", toc)
                     text = text.replace("@content@", content)
+
+
+                    int index = fourthIndex(text)
+                    if (index != -1) {
+                        String start = text.substring(0, index)
+                        String end = text.substring(index, text.length())
+                        text = start + HTML_DIV_COL_MD_8 + end
+                    }
+
                     output.text = text
+
                 } catch (FileNotFoundException ignored) {
                 }
             }
         }
+    }
+
+    private static int fourthIndex(String html) {
+        int first = html.indexOf(HTML_SECT1);
+        if (first != -1) {
+            int second = html.indexOf(HTML_SECT1, first + 1);
+            if (second != -1) {
+                int third = html.indexOf(HTML_SECT1, second + 1);
+                if (third != -1) {
+                    return html.indexOf(HTML_SECT1, third + 1);
+                }
+            }
+        }
+        return -1
     }
 }
