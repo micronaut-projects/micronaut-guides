0f84782455d74b3775eb6b49796905c889801b24
Sergio del Amo <sergio.delamo@softamo.com>
2022-02-08 11:12:43 +0100

    Refactor BookControllerTest
    - don’t inject EmbeddedServer, it is not used in the test.
    - don’t inject BookController, it is not used in the test.
    - To fetch a list use `Argument.listOf`
    - Don’t specify application-json as content type. It is the default.
    - Don’t specify application-json as accept header value. It is the default.
    - Use static imports for Assertions
    - Use UriBuilder to build uris with query value parameters.
    - Use HttpRequest.POST method which takes a string. Remove unncessary usage of UriTemplate.

--- guides/micronaut-openapi-generator-server/java/src/test/java/example/micronaut/controller/BooksControllerTest.java
+++ guides/micronaut-openapi-generator-server/java/src/test/java/example/micronaut/controller/BooksControllerTest.java
@@ -2,95 +2,44 @@ package example.micronaut.controller;
 
 import example.micronaut.model.BookAvailability;
 import example.micronaut.model.BookInfo;
+import io.micronaut.http.uri.UriBuilder;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
-import io.micronaut.runtime.server.EmbeddedServer;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.MutableHttpRequest;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
-import io.micronaut.http.MediaType;
-import io.micronaut.http.uri.UriTemplate;
-import io.micronaut.http.cookie.Cookie;
-import io.micronaut.http.client.multipart.MultipartBody;
 import io.micronaut.core.type.Argument;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import jakarta.inject.Inject;
-import reactor.core.publisher.Mono;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.HashSet;
 
-
-/**
- * API tests for BooksController
- */
-@MicronautTest
+@MicronautTest // <1>
 public class BooksControllerTest {
 
     @Inject
-    EmbeddedServer server;
-
-    @Inject
     @Client("${context-path}")
-    HttpClient client;
+    HttpClient client; // <2>
 
-    @Inject
-    BooksController controller;
-
-    /**
-     * This test is used to check that the API available to client through
-     * '/add' to the features of addBook() works as desired.
-     */
-    //tag::addBook[]
-    @Test // <1>
+    @Test
     void addBookClientApiTest() {
-        // given
-        BookInfo body = new BookInfo("My Book", BookAvailability.fromValue("available")); // <2>
-        String uri = UriTemplate.of("/add").expand(new HashMap<>());
-        MutableHttpRequest<?> request = HttpRequest.POST(uri, body)
-                .contentType("application/json")
-                .accept("application/json");
-
-        // when
-        HttpResponse<?> response = client.toBlocking().exchange(request);
-
-        // then
-        Assertions.assertEquals(HttpStatus.OK, response.status());
+        BookInfo body = new BookInfo("My Book", BookAvailability.AVAILABLE);
+        HttpResponse<?> response = client.toBlocking()
+                .exchange(HttpRequest.POST("/add", body)); // <3>
+        assertEquals(HttpStatus.OK, response.status());
     }
-    //end::addBook[]
 
-    /**
-     * This test is used to check that the API available to client through
-     * '/search' to the features of search() works as desired.
-     */
-    //tag::search[]
-    @Test // <1>
+    @Test
     void searchClientApiTest() {
-        // given
-        String uri = UriTemplate.of("/search").expand(new HashMap<>());
-        MutableHttpRequest<?> request = HttpRequest.GET(uri)
-                .accept("applicaton/json");
-        request.getParameters()
-                .add("book-name", "Guide"); // <2>
-
-        // when
-        HttpResponse<?> response = client.toBlocking().exchange(request, Argument.of(List.class, BookInfo.class));
-        List<BookInfo> body = (List<BookInfo>) response.body(); // <3>
-
-        // then
-        Assertions.assertEquals(HttpStatus.OK, response.status());
-        Assertions.assertEquals(2, body.size()); // <4>
+        HttpResponse<List<BookInfo>> response = client.toBlocking()
+                .exchange(HttpRequest.GET(UriBuilder.of("/search")
+                        .queryParam("book-name", "Guide")
+                        .build()
+                ), Argument.listOf(BookInfo.class)); // <4>
+        List<BookInfo> body = response.body(); // <5>
+        assertEquals(HttpStatus.OK, response.status());
+        assertEquals(2, body.size());
     }
-    //end::search[]
-
 }
