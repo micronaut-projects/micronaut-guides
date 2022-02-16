90e439c3d3d4dbbdc3cbeec053d3cfea3d81c8f4
Andriy Dmytruk <andriy.dmytruk@oracle.com>
2022-02-16 13:21:02 -0500

    Refactor the model tests. Add description to BookInfoTest.

--- guides/micronaut-openapi-generator-server/java/src/test/java/example/micronaut/model/BookInfoTest.java
+++ guides/micronaut-openapi-generator-server/java/src/test/java/example/micronaut/model/BookInfoTest.java
@@ -1,10 +1,13 @@
 package example.micronaut.model;
 
+//tag::imports[]
 import io.micronaut.context.annotation.Property;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.http.HttpRequest;
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
@@ -14,85 +17,62 @@ import org.junit.jupiter.api.Test;
 import javax.validation.Validator;
 
 import static org.junit.jupiter.api.Assertions.*;
+//end::imports[]
 
 /**
  * Model tests for BookInfo
  */
-@Property(name = "spec.name", value = "BookInfoTest")
+//tag::annotations[]
+@Property(name = "spec.name", value = "BookInfoTest") // <2>
 @MicronautTest
 public class BookInfoTest {
+//end::annotations[]
+    //tag::requiredProperties[]
     @Inject
-    Validator validator;
-
-    @Inject
-    @Client("/")
-    HttpClient httpClient;
-
-    @Test
-    public void bookInfoJsonSerialization() {
-        String json = httpClient.toBlocking().retrieve(HttpRequest.GET("/bookinfo"));
-        assertEquals("{\"name\":\"Alice's Adventures in Wonderland\",\"availability\":\"available\",\"author\":\"Lewis Carroll\",\"ISBN\":\"9783161484100\"}", json);
-    }
-
-    @Requires(property = "spec.name", value = "BookInfoTest")
-    @Controller("/bookinfo")
-    static class BookInfoSerdeController {
-        @PermitAll
-        @Get
-        BookInfo index() {
-            return new BookInfo("Alice's Adventures in Wonderland", BookAvailability.AVAILABLE)
-                    .author("Lewis Carroll")
-                    .ISBN("9783161484100");
-        }
-    }
-
-    @Test
-    public void validBookInfo() {
-        BookInfo bookInfo = new BookInfo("Alice's Adventures in Wonderland", BookAvailability.AVAILABLE);
-        assertTrue(validator.validate(bookInfo).isEmpty());
-    }
-
-    /**
-     * Model tests for BookInfo
-     */
-    @Test
-    public void testBookInfo() {
-        BookInfo bookInfo = new BookInfo(null, BookAvailability.AVAILABLE);
-        assertFalse(validator.validate(bookInfo).isEmpty());
-    }
+    Validator validator; // <1>
 
     /**
      * Test the property 'name'
      */
     @Test
     public void nameTest() {
-        BookInfo bookInfo = new BookInfo(null, BookAvailability.AVAILABLE);
-        assertFalse(validator.validate(bookInfo).isEmpty());
+        BookInfo bookInfo = new BookInfo("Alice's Adventures in Wonderland", BookAvailability.AVAILABLE);
+        assertTrue(validator.validate(bookInfo).isEmpty()); // <2>
+
+        bookInfo = new BookInfo(null, BookAvailability.AVAILABLE);
+        assertFalse(validator.validate(bookInfo).isEmpty()); // <3>
     }
 
     /**
      * Test the property 'availability'
      */
     @Test
-    public void availabilityTest() {
-        BookInfo bookInfo = new BookInfo("Alice's Adventures in Wonderland", null);
+    public void availabilityTest() { // <4>
+        BookInfo bookInfo = new BookInfo("ALice's Adventures in Wonderland", BookAvailability.RESERVED);
+        assertTrue(validator.validate(bookInfo).isEmpty());
+
+        bookInfo = new BookInfo("Alice's Adventures in Wonderland", null);
         assertFalse(validator.validate(bookInfo).isEmpty());
     }
+    //end::requiredProperties[]
 
+    //tag::otherProperties[]
     /**
      * Test the property 'author'
      */
     @Test
     public void authorTest() {
         BookInfo bookInfo = new BookInfo("Alice's Adventures in Wonderland", BookAvailability.AVAILABLE)
-                .author("fo");
-        assertFalse(validator.validate(bookInfo).isEmpty());
-        bookInfo = new BookInfo("Alice's Adventures in Wonderland", BookAvailability.AVAILABLE)
-                .author("Lewis Carroll");
-        assertTrue(validator.validate(bookInfo).isEmpty());
-        bookInfo = new BookInfo("Alice's Adventures in Wonderland", BookAvailability.AVAILABLE)
                 .author(null);
         assertTrue(validator.validate(bookInfo).isEmpty());
+
+        bookInfo = new BookInfo("Alice's Adventures in Wonderland", BookAvailability.AVAILABLE)
+                .author("Lewis Carroll");
+        assertTrue(validator.validate(bookInfo).isEmpty()); // <1>
+
+        bookInfo = new BookInfo("Alice's Adventures in Wonderland", BookAvailability.AVAILABLE)
+                .author("fo");
+        assertFalse(validator.validate(bookInfo).isEmpty()); // <2>
     }
 
     /**
@@ -106,10 +86,40 @@ public class BookInfoTest {
 
         bookInfo = new BookInfo("Alice's Adventures in Wonderland", BookAvailability.AVAILABLE)
                 .ISBN("9783161484100");
-        assertTrue(validator.validate(bookInfo).isEmpty());
+        assertTrue(validator.validate(bookInfo).isEmpty()); // <3>
 
         bookInfo = new BookInfo("Alice's Adventures in Wonderland", BookAvailability.AVAILABLE)
-                .ISBN("978316148410");
-        assertFalse(validator.validate(bookInfo).isEmpty());
+                .ISBN("9783161 84100");
+        assertFalse(validator.validate(bookInfo).isEmpty()); // <4>
     }
+    //end::otherProperties[]
+
+    //tag::jsonSerialization[]
+    @Inject
+    @Client("/")
+    HttpClient httpClient;
+
+    @Test
+    public void bookInfoJsonSerialization() {
+        BookInfo requiredBookInfo = new BookInfo("Alice's Adventures in Wonderland", BookAvailability.AVAILABLE)
+                .author("Lewis Carroll")
+                .ISBN("9783161484100");
+
+        BookInfo bookInfo = httpClient.toBlocking().retrieve(HttpRequest.GET("/bookinfo"), BookInfo.class); // <5>
+        assertEquals(requiredBookInfo, bookInfo);
+    }
+
+    @Requires(property = "spec.name", value = "BookInfoTest") // <3>
+    @Controller("/bookinfo") // <1>
+    static class BookInfoSerdeController {
+        @PermitAll
+        @Produces(MediaType.APPLICATION_JSON)
+        @Get
+        BookInfo index() { // <4>
+            return new BookInfo("Alice's Adventures in Wonderland", BookAvailability.AVAILABLE)
+                    .author("Lewis Carroll")
+                    .ISBN("9783161484100");
+        }
+    }
+    //end::jsonSerialization[]
 }
