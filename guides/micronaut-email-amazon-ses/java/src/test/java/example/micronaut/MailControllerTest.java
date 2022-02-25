6967a17e2f7b00143d5fcdb4c7b61fab0de8f96e
Burt Beckwith <burt@burtbeckwith.com>
2022-02-25 03:54:18 -0500

    add Groovy and Kotlin source for micronaut-email-amazon-ses and micronaut-email-sendgrid (#806)
    * cleanup
    
    * move the static inner sender replacement to its own class so it's Kotlin-friendly
    
    * add Groovy and Kotlin source

--- guides/micronaut-email-amazon-ses/java/src/test/java/example/micronaut/MailControllerTest.java
+++ guides/micronaut-email-amazon-ses/java/src/test/java/example/micronaut/MailControllerTest.java
@@ -1,42 +1,27 @@
 package example.micronaut;
 
 import io.micronaut.context.BeanContext;
-import io.micronaut.context.annotation.Replaces;
+import io.micronaut.context.annotation.Property;
 import io.micronaut.core.util.CollectionUtils;
-import io.micronaut.email.AsyncEmailSender;
 import io.micronaut.email.AsyncTransactionalEmailSender;
 import io.micronaut.email.BodyType;
 import io.micronaut.email.Email;
-import io.micronaut.email.EmailException;
-import io.micronaut.email.ses.AsyncSesEmailSender;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
-import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
-import jakarta.inject.Named;
-import jakarta.inject.Singleton;
 import org.junit.jupiter.api.Test;
-import org.reactivestreams.Publisher;
-import reactor.core.publisher.Mono;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.Consumer;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import io.micronaut.context.annotation.Property;
-import io.micronaut.context.annotation.Requires;
-import software.amazon.awssdk.services.ses.model.SendEmailResponse;
-import software.amazon.awssdk.services.ses.model.SesRequest;
-import software.amazon.awssdk.services.ses.model.SesResponse;
 
-@Property(name = "spec.name", value="MailControllerTest") // <1>
+import java.util.Collections;
+
+import static io.micronaut.http.HttpStatus.ACCEPTED;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Property(name = "spec.name", value = "MailControllerTest") // <1>
 @MicronautTest // <2>
 public class MailControllerTest {
 
@@ -49,15 +34,19 @@ public class MailControllerTest {
 
     @Test
     void getMailSendEndpointSendsAnEmail() {
-        HttpResponse<?> response = httpClient.toBlocking().exchange(HttpRequest.POST("/mail/send",
-                Collections.singletonMap("to", "johnsnow@micronaut.example")));
-        assertEquals(HttpStatus.ACCEPTED, response.status());
 
-        AsyncTransactionalEmailSender<?,?> sender = beanContext.getBean(AsyncTransactionalEmailSender.class);
-        assertTrue(sender instanceof AsyncSesEmailSenderReplacement);
-        AsyncSesEmailSenderReplacement sendgridSender = (AsyncSesEmailSenderReplacement) sender;
+        HttpResponse<?> response = httpClient.toBlocking().exchange(
+                HttpRequest.POST("/mail/send",
+                Collections.singletonMap("to", "johnsnow@micronaut.example")));
+        assertEquals(ACCEPTED, response.status());
+
+        AsyncTransactionalEmailSender<?, ?> sender = beanContext.getBean(AsyncTransactionalEmailSender.class);
+        assertTrue(sender instanceof EmailSenderReplacement);
+
+        EmailSenderReplacement sendgridSender = (EmailSenderReplacement) sender;
         assertTrue(CollectionUtils.isNotEmpty(sendgridSender.getEmails()));
         assertEquals(1, sendgridSender.getEmails().size());
+
         Email email = sendgridSender.getEmails().get(0);
         assertEquals(email.getFrom().getEmail(), "john@micronaut.example");
         assertNotNull(email.getTo());
@@ -67,28 +56,5 @@ public class MailControllerTest {
         assertNotNull(email.getBody());
         assertTrue(email.getBody().get(BodyType.HTML).isPresent());
         assertEquals(email.getBody().get(BodyType.HTML).get(), "and <em>easy</em> to do anywhere with <strong>Micronaut Email</strong>");
-    }
-
-    @Requires(property = "spec.name", value="MailControllerTest") // <1>
-    @Singleton
-    @Replaces(AsyncSesEmailSender.class)
-    @Named(AsyncSesEmailSender.NAME)
-    static class AsyncSesEmailSenderReplacement implements AsyncTransactionalEmailSender<SesRequest, SesResponse> {
-        private final List<Email> emails = new ArrayList<>();
-
-        @Override
-        public String getName() {
-            return AsyncSesEmailSender.NAME;
-        }
-
-        @Override
-        public Publisher<SesResponse> sendAsync(@NotNull @Valid Email email, @NotNull Consumer<SesRequest> emailRequest) throws EmailException {
-            emails.add(email);
-            return Mono.just(SendEmailResponse.builder().messageId("xxx-yyy-zzz").build());
-        }
-
-        public List<Email> getEmails() {
-            return emails;
-        }
     }
 }
---
+++ guides/micronaut-email-sendgrid/kotlin/src/test/kotlin/example/micronaut/EmailSenderReplacement.kt
@@ -0,0 +1,41 @@
+package example.micronaut
+
+import com.sendgrid.Request
+import com.sendgrid.Response
+import io.micronaut.context.annotation.Replaces
+import io.micronaut.context.annotation.Requires
+import io.micronaut.email.AsyncEmailSender
+import io.micronaut.email.AsyncTransactionalEmailSender
+import io.micronaut.email.Email
+import io.micronaut.email.EmailException
+import io.micronaut.http.HttpStatus.ACCEPTED
+import jakarta.inject.Named
+import jakarta.inject.Singleton
+import org.reactivestreams.Publisher
+import reactor.core.publisher.Mono
+import java.util.function.Consumer
+import javax.validation.Valid
+
+@Requires(property = "spec.name", value = "MailControllerTest") // <1>
+@Singleton
+@Replaces(AsyncEmailSender::class)
+@Named(EmailSenderReplacement.NAME)
+open class EmailSenderReplacement : AsyncTransactionalEmailSender<Request, Response> {
+
+    val emails = mutableListOf<Email>()
+
+    override fun getName(): String = NAME
+
+    @Throws(EmailException::class)
+    override fun sendAsync(@Valid email: Email,
+                           emailRequest: Consumer<Request>): Publisher<Response> {
+        emails.add(email)
+        val response = Response()
+        response.statusCode = ACCEPTED.code
+        return Mono.just(response)
+    }
+
+    companion object {
+        const val NAME = "sendgrid"
+    }
+}
