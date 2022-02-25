c29d2d0a988feb571da41e65d645a43816df6435
Burt Beckwith <burt@burtbeckwith.com>
2022-02-24 21:33:32 -0500

    move MockFruitRepository to its own class so it's Kotlin-friendly

--- guides/micronaut-mongodb-asynchronous/java/src/test/java/example/micronaut/FruitControllerValidationTest.java
+++ guides/micronaut-mongodb-asynchronous/java/src/test/java/example/micronaut/FruitControllerValidationTest.java
@@ -1,19 +1,13 @@
 package example.micronaut;
 
 import io.micronaut.context.annotation.Property;
-import io.micronaut.context.annotation.Replaces;
-import io.micronaut.context.annotation.Requires;
-import io.micronaut.core.async.publisher.Publishers;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
-import jakarta.inject.Singleton;
 import org.junit.jupiter.api.Test;
-import org.reactivestreams.Publisher;
-import reactor.core.publisher.Mono;
 
 import static io.micronaut.http.HttpStatus.BAD_REQUEST;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -32,21 +26,5 @@ public class FruitControllerValidationTest {
         HttpClientResponseException e = assertThrows(HttpClientResponseException.class, () ->
                 httpClient.toBlocking().exchange(HttpRequest.POST("/fruits", new Fruit("", "Hola"))));
         assertEquals(BAD_REQUEST, e.getStatus());
-    }
-
-    @Requires(property = "spec.name", value = "FruitControllerValidationTest")
-    @Singleton
-    @Replaces(FruitRepository.class)
-    static class MockFruitRepository implements FruitRepository {
-
-        @Override
-        public Publisher<Fruit> list() {
-            return Publishers.empty();
-        }
-
-        @Override
-        public Mono<Boolean> save(Fruit fruit) {
-            return Mono.just(false);
-        }
     }
 }
