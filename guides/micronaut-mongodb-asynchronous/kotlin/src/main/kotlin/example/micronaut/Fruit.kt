9bcd743cfd1799b9eac8acdd8c514a947ad8fa90
Tim Yates <tim.yates@gmail.com>
2022-02-28 10:15:02 +0000

    Fix callouts in mongo POJO kotlin class
    Also, make sure code changes invalidate the asciidoc generation

--- guides/micronaut-mongodb-asynchronous/kotlin/src/main/kotlin/example/micronaut/Fruit.kt
+++ guides/micronaut-mongodb-asynchronous/kotlin/src/main/kotlin/example/micronaut/Fruit.kt
@@ -7,9 +7,9 @@ import org.bson.codecs.pojo.annotations.BsonProperty
 import javax.validation.constraints.NotBlank
 
 @Introspected // <1>
-data class Fruit @Creator @BsonCreator constructor( // <4> // <5>
-        @field:BsonProperty("name") @param:BsonProperty("name") @field:NotBlank val name: String, // <2> // <3>
-        @field:BsonProperty("description") @param:BsonProperty("description") var description: String?) { // <3>
+data class Fruit @Creator @BsonCreator constructor( // <4>
+    @field:BsonProperty("name") @param:BsonProperty("name") @field:NotBlank val name: String, // <2> <3>
+    @field:BsonProperty("description") @param:BsonProperty("description") var description: String?) { // <3>
 
     constructor(name: String) : this(name, null)
 }
--- guides/micronaut-mongodb-synchronous/kotlin/src/main/kotlin/example/micronaut/Fruit.kt
+++ guides/micronaut-mongodb-synchronous/kotlin/src/main/kotlin/example/micronaut/Fruit.kt
@@ -8,7 +8,7 @@ import javax.validation.constraints.NotBlank
 
 @Introspected // <1>
 data class Fruit @Creator @BsonCreator constructor( // <4>
-    @field:BsonProperty("name") @param:BsonProperty("name") @field:NotBlank val name: String, // <2> // <3>
+    @field:BsonProperty("name") @param:BsonProperty("name") @field:NotBlank val name: String, // <2> <3>
     @field:BsonProperty("description") @param:BsonProperty("description") var description: String?) { // <3>
 
     constructor(name: String) : this(name, null)
