5c41aa31008846e735f642b87140c88d9db38e68
Tim Yates <tim.yates@gmail.com>
2022-03-16 15:22:40 +0000

    Fix the up-to-date check on the guide tests (#853)

--- buildSrc/src/main/groovy/io/micronaut/guides/tasks/TestScriptTask.groovy
+++ buildSrc/src/main/groovy/io/micronaut/guides/tasks/TestScriptTask.groovy
@@ -5,9 +5,11 @@ import io.micronaut.guides.GuideMetadata
 import io.micronaut.guides.TestScriptGenerator
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
 @CompileStatic
@@ -17,11 +19,11 @@ abstract class TestScriptTask extends DefaultTask {
     @Internal
     GuideMetadata metadata
 
-    @OutputDirectory
-    abstract DirectoryProperty getOutputDir()
+    @OutputFile
+    abstract RegularFileProperty getScriptFile()
 
     @TaskAction
     def perform() {
-        TestScriptGenerator.generateTestScript(outputDir.get().asFile, Collections.singletonList(metadata), false)
+        TestScriptGenerator.generateTestScript(scriptFile.get().asFile.parentFile, Collections.singletonList(metadata), false)
     }
 }
