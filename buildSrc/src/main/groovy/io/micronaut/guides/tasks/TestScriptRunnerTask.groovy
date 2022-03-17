97c1b84da2897483dbeed85bb1a1fdbea5d6941e
Tim Yates <tim.yates@gmail.com>
2022-03-17 11:53:01 +0000

    Use a worker for running the script and make cacheable (#855)

--- buildSrc/src/main/groovy/io/micronaut/guides/tasks/TestScriptRunnerTask.groovy
+++ buildSrc/src/main/groovy/io/micronaut/guides/tasks/TestScriptRunnerTask.groovy
@@ -1,24 +1,26 @@
 package io.micronaut.guides.tasks
 
 import groovy.transform.CompileStatic
-import io.micronaut.guides.GuideMetadata
-import io.micronaut.guides.TestScriptGenerator
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
 import org.gradle.api.tasks.CacheableTask
-import org.gradle.api.tasks.Exec
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.Internal
-import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
+import org.gradle.workers.WorkQueue
+import org.gradle.workers.WorkerExecutor
 
+import javax.inject.Inject
+
+@CacheableTask
 @CompileStatic
-abstract class TestScriptRunnerTask extends Exec {
+abstract class TestScriptRunnerTask extends DefaultTask {
 
     @InputFile
     @PathSensitive(PathSensitivity.RELATIVE)
@@ -30,4 +32,25 @@ abstract class TestScriptRunnerTask extends Exec {
 
     @OutputFile
     abstract RegularFileProperty getOutputFile()
+
+    @Inject
+    abstract WorkerExecutor getWorkerExecutor();
+
+    // We have to wait for the first task to complete as mvnw corrupts the wrapper if downloaded in parallel
+    @Internal
+    abstract Property<Boolean> getAwait()
+
+    @TaskAction
+    void runScript() {
+        WorkQueue queue = workerExecutor.noIsolation()
+
+        queue.submit(TestScriptRunnerWorkAction) { parameters ->
+            parameters.testScript.set(testScript)
+            parameters.outputFile.set(outputFile)
+        }
+
+        // Just run one at a time for now, when we try to parallelize we will need to await if
+        // getAwait() is true, so that the maven wrapper is downloaded uncorrupted.
+        queue.await()
+    }
 }
