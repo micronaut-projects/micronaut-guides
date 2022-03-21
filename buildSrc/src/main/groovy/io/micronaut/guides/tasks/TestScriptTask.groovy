26fae1f2f915a3630635fb7282c8ef67bab50e63
Tim Yates <tim.yates@gmail.com>
2022-03-21 12:47:03 +0000

    Add metadata to runScript generation task
    Otherwise, when a new language combination is added, the run script doesn't
    get re-generated, so the new language doesn't get executed

--- buildSrc/src/main/groovy/io/micronaut/guides/tasks/TestScriptTask.groovy
+++ buildSrc/src/main/groovy/io/micronaut/guides/tasks/TestScriptTask.groovy
@@ -9,9 +9,12 @@ import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 
 @CompileStatic
@@ -20,6 +23,10 @@ abstract class TestScriptTask extends DefaultTask {
 
     @Input
     abstract Property<String> getGuideSlug()
+
+    @InputFile
+    @PathSensitive(PathSensitivity.RELATIVE)
+    abstract RegularFileProperty getMetadataFile()
 
     @Internal
     GuideMetadata metadata
