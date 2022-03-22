fa361c176a0e5a14b7892976babae124e8370c6c
Tim Yates <tim.yates@gmail.com>
2022-03-22 11:13:41 +0000

    Add Kotlin version of the GraphQL ToDo guide
    This adds the Kotlin version of the GraphQL ToDo guide.
    
    I've used ideomatic Kotlin, but found a couple of oddities:
    
    - `AuthorRepository` has to be an abstract class, not an interface or compilation fails
    - `CreateToDoDataFetcher` has to be declared as `open` or `@Transactional` will fail to be applied

---
+++ guides/micronaut-graphql-todo/kotlin/src/main/kotlin/example/micronaut/Author.kt
@@ -0,0 +1,15 @@
+package example.micronaut
+
+import io.micronaut.data.annotation.GeneratedValue
+import io.micronaut.data.annotation.GeneratedValue.Type.AUTO
+import io.micronaut.data.annotation.Id
+import io.micronaut.data.annotation.MappedEntity
+import javax.validation.constraints.NotNull
+
+@MappedEntity // <1>
+class Author(val username: @NotNull String?) {
+
+    @Id // <2>
+    @GeneratedValue(AUTO)
+    var id: Long? = null
+}
