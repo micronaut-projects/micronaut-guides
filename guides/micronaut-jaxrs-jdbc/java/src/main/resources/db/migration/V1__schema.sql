4facd8680f0d71371ab64da6771478be8668fd92
Sergio del Amo <sergio.delamo@softamo.com>
2022-04-05 11:15:58 +0200

    use 4 spaces

--- guides/micronaut-jaxrs-jdbc/java/src/main/resources/db/migration/V1__schema.sql
+++ guides/micronaut-jaxrs-jdbc/java/src/main/resources/db/migration/V1__schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS pet;
 
 CREATE TABLE pet (
-                     id   BIGINT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-                     name  VARCHAR(255) NOT NULL UNIQUE,
-                     type varchar(255) check (type in ('DOG', 'CAT'))
+    id   BIGINT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    name  VARCHAR(255) NOT NULL UNIQUE,
+    type varchar(255) check (type in ('DOG', 'CAT'))
 );
