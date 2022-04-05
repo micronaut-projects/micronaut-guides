67e29d75c5301a002d4b7f127ce46135ae2bd301
Sergio del Amo <sergio.delamo@softamo.com>
2022-04-05 11:13:30 +0200

    remove unused Generated

--- guides/micronaut-jaxrs-jdbc/java/src/main/java/example/micronaut/Pet.java
+++ guides/micronaut-jaxrs-jdbc/java/src/main/java/example/micronaut/Pet.java
@@ -1,6 +1,5 @@
 package example.micronaut;
 
-import io.micronaut.core.annotation.Generated;
 import io.micronaut.core.annotation.NonNull;
 import io.micronaut.data.annotation.GeneratedValue;
 import io.micronaut.data.annotation.Id;
