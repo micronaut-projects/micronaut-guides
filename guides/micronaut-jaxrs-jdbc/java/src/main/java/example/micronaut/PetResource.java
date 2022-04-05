f4bd703da1d858ff6705404ac439513e1a197111
Dan Hollingsworth <hollingsworthd@objectcomputing.com>
2022-04-05 19:46:05 -0400

    Address code review feedback

--- guides/micronaut-jaxrs-jdbc/java/src/main/java/example/micronaut/PetResource.java
+++ guides/micronaut-jaxrs-jdbc/java/src/main/java/example/micronaut/PetResource.java
@@ -23,19 +23,19 @@ class PetResource {
     }
 
     @GET // <3>
-    List<NameDto> all() { // <4>
+    public List<NameDto> all() { // <4>
         return petRepository.list();
     }
 
     @GET // <3>
     @Path("/{name}") // <5>
-    Optional<Pet> byName(@PathParam("name") String petsName) { // <6>
+    public Optional<Pet> byName(@PathParam("name") String petsName) { // <6>
         return petRepository.findByName(petsName);
     }
 
     @POST
     @Status(HttpStatus.CREATED)
-    void save(@NonNull @NotNull @Body PetSave petSave) {
+    public void save(@NonNull @NotNull @Body PetSave petSave) {
         petRepository.save(petSave.getName(), petSave.getType());
 
     }
