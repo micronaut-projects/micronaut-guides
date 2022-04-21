87bdbb96dc66e420bf6c2702abd3226db5458bb5
Sergio del Amo <sergio.delamo@softamo.com>
2022-04-21 16:02:27 +0200

    rest-assured guide

---
+++ guides/micronaut-rest-assured/java/src/test/java/example/micronaut/HelloControllerTest.java
@@ -0,0 +1,27 @@
+package example.micronaut;
+
+import io.micronaut.runtime.server.EmbeddedServer;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@MicronautTest // <1>
+public class HelloControllerTest {
+
+    @Inject
+    EmbeddedServer embeddedServer; // <2>
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+                .port(embeddedServer.getPort()) // <3>
+                .when()
+                .get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("Hello World"));
+    }
+
+}
