0703fb6ce4db93d13476aa04cdf5fa382f5731b2
Dan Hollingsworth <dan@machinepublishers.com>
2022-05-11 05:01:06 -0400

    Guide JAX-RS (#876)
    * jax-rs guide
    
    To build the guide without executing the tests run:
    
     ./gradlew -x micronautJaxrsJdbcRunTestScript micronautJaxrsJdbcBuild --console=plain
    
    open open build/dist/index.html
    
    To run and exectute the tests:
    
     ./gradlew micronautJaxrsJdbcBuild --console=plain
    
    * Add JAX-RS documentation (#866)
    
    * jaxrs 101 guide
    
    * move enum before entity
    
    * remove unused NotNull
    
    * remove unused Generated
    
    * use 4 spaces
    
    * remove unused import
    
    * don't use wildcard imports
    
    * better intro
    
    * change copy of common flyway
    
    from:
    
    Add the following snippet to include the necessary dependencies:
    
    to:
    
    The `flyway` feature adds the following dependency:
    
    * remove unncessary blank lines
    
    * instructions to run the app
    
    * explain guides goal
    
    * Address code review feedback
    
    * Update guides/micronaut-jaxrs-jdbc/micronaut-jaxrs-jdbc.adoc
    
    Co-authored-by: Kristin Kroeger <33693760+kroegerk@users.noreply.github.com>
    
    * Update src/docs/common/snippets/common-datasource-configuration.adoc
    
    Co-authored-by: Kristin Kroeger <33693760+kroegerk@users.noreply.github.com>
    
    * Address code review feedback
    
    * add @MicronautTest(transactional = false) back
    
    * external common-datasource-configuration
    
    * rename to PetSave otherwise it does not compile
    
    * add twitter card
    
    * add sergio as author
    
    * set publication date to May 5th
    
    Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>
    Co-authored-by: Kristin Kroeger <33693760+kroegerk@users.noreply.github.com>

---
+++ guides/micronaut-jaxrs-jdbc/java/src/main/java/example/micronaut/PetRepository.java
@@ -0,0 +1,23 @@
+package example.micronaut;
+
+import io.micronaut.data.jdbc.annotation.JdbcRepository;
+import io.micronaut.data.model.query.builder.sql.Dialect;
+import io.micronaut.data.repository.CrudRepository;
+import java.util.List;
+import java.util.Optional;
+import io.micronaut.core.annotation.NonNull;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@JdbcRepository(dialect = Dialect.MYSQL) // <1>
+public interface PetRepository extends CrudRepository<Pet, Long> { // <2>
+
+    @NonNull
+    List<NameDto> list(); // <3>
+
+    @NonNull
+    Optional<Pet> findByName(@NonNull @NotBlank String name);
+
+    Pet save(@NonNull @NotBlank String name,
+             @NonNull @NotNull PetType type);
+}
