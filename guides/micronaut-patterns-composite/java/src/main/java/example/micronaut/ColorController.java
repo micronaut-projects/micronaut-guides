71429c20503120cdfc5007729414c572b7db1e60
Burt Beckwith <burt@burtbeckwith.com>
2022-05-13 01:48:02 -0400

    Groovy and Kotlin source for micronaut-patterns-composite (#925)

--- guides/micronaut-patterns-composite/java/src/main/java/example/micronaut/ColorController.java
+++ guides/micronaut-patterns-composite/java/src/main/java/example/micronaut/ColorController.java
@@ -2,10 +2,11 @@ package example.micronaut;
 
 import io.micronaut.core.annotation.NonNull;
 import io.micronaut.http.HttpRequest;
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
-import io.micronaut.http.MediaType;
+
 import java.util.Optional;
 
 @Controller("/color") // <1>
