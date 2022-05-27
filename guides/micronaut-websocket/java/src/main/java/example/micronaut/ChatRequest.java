b5e781144e4b98fb54bbdb347b05509c6d15d7b0
Dan Hollingsworth <hollingsworthd@objectcomputing.com>
2022-05-27 10:38:55 -0400

    Remove unused field

--- guides/micronaut-websocket/java/src/main/java/example/micronaut/ChatRequest.java
+++ guides/micronaut-websocket/java/src/main/java/example/micronaut/ChatRequest.java
@@ -7,20 +7,14 @@ public class ChatRequest {
 
     private final String username; // <2>
     private final String topic; // <3>
-    private final String message; // <4>
 
     public ChatRequest() {
-        this(null, null, null);
+        this(null, null);
     }
 
     public ChatRequest(String username, String topic) {
-        this(username, topic, null);
-    }
-
-    public ChatRequest(String username, String topic, String message) {
         this.username = username;
         this.topic = topic;
-        this.message = message;
     }
 
     public String getUsername() {
@@ -29,9 +23,5 @@ public class ChatRequest {
 
     public String getTopic() {
         return topic;
-    }
-
-    public String getMessage() {
-        return message;
     }
 }
