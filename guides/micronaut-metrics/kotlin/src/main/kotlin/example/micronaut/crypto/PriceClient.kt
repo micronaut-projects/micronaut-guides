8905d2f7a947c494ae5f1f2fa8231a9ab2ff2215
Sergio del Amo <sergio.delamo@softamo.com>
2022-06-10 17:46:46 +0200

    add missing imports

--- guides/micronaut-metrics/kotlin/src/main/kotlin/example/micronaut/crypto/PriceClient.kt
+++ guides/micronaut-metrics/kotlin/src/main/kotlin/example/micronaut/crypto/PriceClient.kt
@@ -1,5 +1,9 @@
 package example.micronaut.crypto
 
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.QueryValue
+import io.micronaut.http.client.annotation.Client
+
 @Client(id = "kucoin") // <1>
 abstract class PriceClient {
     @Get("/api/v1/market/orderbook/level1")
