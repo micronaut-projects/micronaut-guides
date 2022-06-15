1f91fab51f3391493c7f47bb9bf121cbe0c71876
Sergio del Amo <sergio.delamo@softamo.com>
2022-06-15 18:24:10 +0200

    move example of spring web to guide (#839)
    * move example of spring web to guide
    
    To generate the sample projects and the HTML
    
    `./gradlew micronautSpringWebBuild`
    
    You can open the HTML at:
    
    `open build/dist/index.html`
    
    To run the test for every generated project:
    
    ```
    cd build/code/micronaut-spring-web
    ./test.sh
    ```
    
    * guide
    
    * Tweaks
    
    - Add configuration to guide (the GreetingControllerTest fails without it)
    - AtomicReference in the Service can be final
    - Test classes do not need to be public
    
    * Update guides/micronaut-spring-boot/micronaut-spring-boot.adoc
    
    Co-authored-by: Kristin Kroeger <33693760+kroegerk@users.noreply.github.com>
    
    * Update guides/micronaut-spring-boot/micronaut-spring-boot.adoc
    
    Co-authored-by: Kristin Kroeger <33693760+kroegerk@users.noreply.github.com>
    
    * Update guides/micronaut-spring-boot/micronaut-spring-boot.adoc
    
    Co-authored-by: Kristin Kroeger <33693760+kroegerk@users.noreply.github.com>
    
    * Update guides/micronaut-spring-boot/micronaut-spring-boot.adoc
    
    Co-authored-by: Kristin Kroeger <33693760+kroegerk@users.noreply.github.com>
    
    * Update guides/micronaut-spring-boot/micronaut-spring-boot.adoc
    
    Co-authored-by: Kristin Kroeger <33693760+kroegerk@users.noreply.github.com>
    
    * Update guides/micronaut-spring-boot/micronaut-spring-boot.adoc
    
    Co-authored-by: Kristin Kroeger <33693760+kroegerk@users.noreply.github.com>
    
    * Update src/docs/common/callouts/callout-spring-ann-scheduled.adoc
    
    Co-authored-by: Kristin Kroeger <33693760+kroegerk@users.noreply.github.com>
    
    * Update src/docs/common/callouts/callout-spring-response-entity.adoc
    
    Co-authored-by: Kristin Kroeger <33693760+kroegerk@users.noreply.github.com>
    
    * Update src/docs/common/callouts/callout-spring-ann-service.adoc
    
    Co-authored-by: Kristin Kroeger <33693760+kroegerk@users.noreply.github.com>
    
    Co-authored-by: Tim Yates <tim.yates@gmail.com>
    Co-authored-by: Kristin Kroeger <33693760+kroegerk@users.noreply.github.com>

---
+++ guides/micronaut-spring-boot/java/src/test/java/example/micronaut/GreetingControllerPostTest.java
@@ -0,0 +1,25 @@
+package example.micronaut;
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@MicronautTest
+class GreetingControllerPostTest {
+
+    @Inject
+    GreetingClient greetingClient;
+
+    @Test
+    void postWithRequestBodyAndPostMappingWorks() {
+        Greeting request = new Greeting(99l, "Sergio");
+        Greeting response = greetingClient.greetByPost(request) ;
+        assertNotNull(response);
+        assertNotEquals(request.getId(), response.getId());
+        assertEquals("Hola, " + request.getContent() + "!", response.getContent());
+    }
+}
