f762a1185c8e859578f749f49d499853ff3f4d14
Sergio del Amo <sergio.delamo@softamo.com>
2022-06-17 18:12:57 +0200

    remove application and logback.xml
    These are generated automatically

--- guides/micronaut-turbo-native/groovy/example/micronaut/Application.groovy
+++
@@ -1,9 +0,0 @@
-package example.micronaut
-
-import io.micronaut.runtime.Micronaut
-
-class Application {
-    static void main(String[] args) {
-        Micronaut.run(Application.class, args)
-    }
-}
