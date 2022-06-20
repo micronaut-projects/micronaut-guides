1ea25f50ac0180b26b9d3323cf9d8bbb406e7cfa
Tim Yates <tim.yates@gmail.com>
2022-06-20 12:58:59 +0100

    Fix code

--- guides/micronaut-hashicorp-vault/kotlin/src/main/kotlin/example/micronaut/ClientIdController.kt
+++ guides/micronaut-hashicorp-vault/kotlin/src/main/kotlin/example/micronaut/ClientIdController.kt
@@ -3,9 +3,11 @@ package example.micronaut
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.oauth2.configuration.OauthClientConfiguration
 import io.micronaut.security.rules.SecurityRule
-import javax.inject.Named
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Produces
+import jakarta.inject.Named
 
 @Controller
 class ClientIdController(@Named("companyauthserver") oauthClientConfiguration: OauthClientConfiguration) {
