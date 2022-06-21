a6439fe18548e628c98d27839a3269f7e397323c
Sergio del Amo <sergio.delamo@softamo.com>
2022-06-21 17:21:41 +0200

    remove section titles of categories (#986)
    @JasonTypesCodes :
    
    > The other sections have two titles.  One in the card on the left and again at the top of the table.  I think we can probably remove the title from the top of the remaining tables.

--- buildSrc/src/main/groovy/io/micronaut/guides/IndexGenerator.groovy
+++ buildSrc/src/main/groovy/io/micronaut/guides/IndexGenerator.groovy
@@ -214,7 +214,7 @@ class IndexGenerator {
             }
         } else {
             index += "<div class='col-sm-8'>"
-            index += guidesTable(filteredMetadatas, cat.toString() + " Guides", false)
+            index += guidesTable(filteredMetadatas)
             index += "</div>"
         }
 
