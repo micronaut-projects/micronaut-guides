0bb6552804794ff9b9bd1850c54f0352ea964a93
Tim Yates <tim.yates@gmail.com>
2022-06-29 09:39:45 +0100

    [skip ci] If no env var, detect java version properly

--- buildSrc/src/main/groovy/io/micronaut/guides/Utils.groovy
+++ buildSrc/src/main/groovy/io/micronaut/guides/Utils.groovy
@@ -3,13 +3,13 @@ package io.micronaut.guides
 import groovy.transform.CompileStatic
 import io.micronaut.starter.options.JdkVersion
 import org.gradle.api.GradleException
+import org.gradle.api.JavaVersion
 
 @CompileStatic
 class Utils {
 
     private static final String SYS_PROP_MICRONAUT_GUIDE = 'micronaut.guide'
     private static final String ENV_JDK_VERSION = 'JDK_VERSION'
-    private static final JdkVersion DEFAULT_JAVA_VERSION = JdkVersion.JDK_11
 
     static String singleGuide() {
         System.getProperty(SYS_PROP_MICRONAUT_GUIDE)
@@ -45,7 +45,7 @@ class Utils {
     }
 
     static JdkVersion parseJdkVersion() {
-        JdkVersion javaVersion = DEFAULT_JAVA_VERSION
+        JdkVersion javaVersion
         if (System.getenv(ENV_JDK_VERSION)) {
             try {
                 int majorVersion = Integer.valueOf(System.getenv(ENV_JDK_VERSION))
@@ -53,6 +53,8 @@ class Utils {
             } catch (NumberFormatException ignored) {
                 throw new GradleException("Could not parse env " + ENV_JDK_VERSION + " to JdkVersion")
             }
+        } else {
+            javaVersion = JdkVersion.valueOf(JavaVersion.current().majorVersion as Integer)
         }
         javaVersion
     }
