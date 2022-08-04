494b2b1bc345eb2adb715ef9c0ffe98f454d6936
Sergio del Amo <sergio.delamo@softamo.com>
2022-08-04 09:12:30 +0200

    hibernate reactive with Micronaut data (#1002)
    Co-authored-by: Tim Yates <tim.yates@gmail.com>

---
+++ guides/micronaut-data-hibernate-reactive/java/src/main/java/example/micronaut/GenreRepository.java
@@ -0,0 +1,29 @@
+package example.micronaut;
+
+import example.micronaut.domain.Genre;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.data.annotation.Id;
+import io.micronaut.data.annotation.Repository;
+import io.micronaut.data.exceptions.DataAccessException;
+import io.micronaut.data.repository.reactive.ReactorPageableRepository;
+import reactor.core.publisher.Mono;
+
+import javax.transaction.Transactional;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Repository // <1>
+public interface GenreRepository extends ReactorPageableRepository<Genre, Long> { // <2>
+
+    Mono<Genre> save(@NonNull @NotBlank String name);
+
+    @Transactional
+    default Mono<Genre> saveWithException(@NonNull @NotBlank String name) {
+        return save(name)
+                .handle((genre, sink) -> {
+                    sink.error(new DataAccessException("test exception"));
+                });
+    }
+
+    Mono<Long> update(@NonNull @NotNull @Id Long id, @NonNull @NotBlank String name);
+}
