5d1cc0165e36ce5c7846922a883626926afca7d3
Sergio del Amo <sergio.delamo@softamo.com>
2022-08-04 11:58:48 +0200

    add DefaultPomDependencyVersionResolverReplacement
    see: https://github.com/micronaut-projects/micronaut-starter/pull/1287

---
+++ buildSrc/src/main/java/io/micronaut/guides/DefaultPomDependencyVersionResolverReplacement.java
@@ -0,0 +1,26 @@
+package io.micronaut.guides;
+
+import io.micronaut.context.annotation.Replaces;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.core.io.ResourceResolver;
+import io.micronaut.starter.build.dependencies.Coordinate;
+import io.micronaut.starter.build.dependencies.DefaultPomDependencyVersionResolver;
+import jakarta.inject.Singleton;
+import java.util.Map;
+import java.util.Optional;
+import static io.micronaut.starter.build.dependencies.CoordinatesUtils.readCoordinates;
+
+@Replaces(DefaultPomDependencyVersionResolver.class)
+@Singleton
+public class DefaultPomDependencyVersionResolverReplacement extends DefaultPomDependencyVersionResolver {
+    private final Map<String, Coordinate> coordinates;
+    public DefaultPomDependencyVersionResolverReplacement(ResourceResolver resourceResolver) {
+        coordinates = readCoordinates(resourceResolver.getResources("classpath:pom.xml"));
+    }
+
+    @Override
+    @NonNull
+    public Optional<Coordinate> resolve(@NonNull String artifactId) {
+        return Optional.ofNullable(coordinates.get(artifactId));
+    }
+}
