f25991edfb5689d9b603a65f303ab2a7e7262e79
Nemanja Mikic <nemanja.mikic@oracle.com>
2022-08-04 09:53:42 +0200

    Opentelemetry OCI guide (#1017)

---
+++ guides/micronaut-cloud-trace-base/kotlin/src/main/kotlin/example/micronaut/WarehouseController.kt
@@ -0,0 +1,30 @@
+package example.micronaut
+
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
+import io.micronaut.scheduling.TaskExecutors
+import io.micronaut.scheduling.annotation.ExecuteOn
+import java.util.Random
+
+@ExecuteOn(TaskExecutors.IO)
+@Controller("/warehouse")
+class WarehouseController {
+
+    @Get("/count")
+    fun getItemCount() : HttpResponse<Int> = HttpResponse.ok(Random().nextInt(11))
+
+
+    @Post("/order")
+    fun order() : HttpResponse<Any> {
+        try {
+            //To simulate an external process taking time
+            Thread.sleep(500)
+        } catch (e: InterruptedException) {
+        }
+
+        return HttpResponse.accepted()
+    }
+
+}
