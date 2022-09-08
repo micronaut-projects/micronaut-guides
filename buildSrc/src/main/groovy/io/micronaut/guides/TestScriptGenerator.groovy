72b214497515cd84da9ba5b743cbf139d75b6304
Tim Yates <tim.yates@gmail.com>
2022-09-08 13:39:08 +0100

    Stop shared test resources server after running each test

--- buildSrc/src/main/groovy/io/micronaut/guides/TestScriptGenerator.groovy
+++ buildSrc/src/main/groovy/io/micronaut/guides/TestScriptGenerator.groovy
@@ -159,6 +159,8 @@ cd $nestedFolder
 echo "-------------------------------------------------"
 echo "Executing '$folder' tests"
 ${buildTool == MAVEN ? './mvnw -q test' : './gradlew -q test' } || EXIT_STATUS=\$?
+echo "Stopping shared test resources service (if created)"
+${buildTool == MAVEN ? './mvnw -q mn:stop-testresources-service' : './gradlew -q stopTestResourcesService'} || true
 cd ..
 """
         if (stopIfFailure) {
