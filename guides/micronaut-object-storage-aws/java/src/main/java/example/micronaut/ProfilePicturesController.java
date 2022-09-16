8f4839a26cbba8257cbe1383ef9e6fba55cb1cc2
Sergio del Amo <sergio.delamo@softamo.com>
2022-09-16 10:07:20 +0200

    Improvements to Object Storage Guide (#1092)
    Co-authored-by: Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>

--- guides/micronaut-object-storage-aws/java/src/main/java/example/micronaut/ProfilePicturesController.java
+++ guides/micronaut-object-storage-aws/java/src/main/java/example/micronaut/ProfilePicturesController.java
@@ -1,17 +1,22 @@
 package example.micronaut;
 
+import io.micronaut.http.HttpHeaders;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.MutableHttpResponse;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Delete;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
 import io.micronaut.http.multipart.CompletedFileUpload;
 import io.micronaut.http.server.types.files.StreamedFile;
+import io.micronaut.http.server.util.HttpHostResolver;
+import io.micronaut.http.uri.UriBuilder;
 import io.micronaut.objectstorage.aws.AwsS3ObjectStorageEntry;
 import io.micronaut.objectstorage.aws.AwsS3Operations;
 import io.micronaut.objectstorage.request.UploadRequest;
 import io.micronaut.objectstorage.response.UploadResponse;
-import io.micronaut.runtime.server.EmbeddedServer;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
 import software.amazon.awssdk.services.s3.model.GetObjectResponse;
@@ -19,8 +24,6 @@ import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
 import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.Optional;
 
 //tag::begin-class[]
@@ -29,20 +32,19 @@ import java.util.Optional;
 public class ProfilePicturesController implements ProfilePicturesApi {
 
     static final String PREFIX = "/pictures";
-    private static final String ETAG_HEADER = "ETag";
 
     private final AwsS3Operations objectStorage; // <3>
-    private final EmbeddedServer server; // <4>
+    private final HttpHostResolver httpHostResolver; // <4>
 
-    public ProfilePicturesController(AwsS3Operations objectStorage, EmbeddedServer server) {
+    public ProfilePicturesController(AwsS3Operations objectStorage, HttpHostResolver httpHostResolver) {
         this.objectStorage = objectStorage;
-        this.server = server;
+        this.httpHostResolver = httpHostResolver;
     }
 //end::begin-class[]
 
     //tag::upload[]
     @Override
-    public HttpResponse upload(CompletedFileUpload fileUpload, String userId) {
+    public HttpResponse<?> upload(CompletedFileUpload fileUpload, String userId, HttpRequest<?> request) {
         String key = buildKey(userId); // <1>
         UploadRequest objectStorageUpload = UploadRequest.fromCompletedFileUpload(fileUpload, key); // <2>
         UploadResponse<PutObjectResponse> response = objectStorage.upload(objectStorageUpload, builder -> {  // <3>
@@ -50,24 +52,21 @@ public class ProfilePicturesController implements ProfilePicturesApi {
         });
 
         return HttpResponse
-                .created(location(userId)) // <5>
-                .header(ETAG_HEADER, response.getETag()); // <6>
+                .created(location(request, userId)) // <5>
+                .header(HttpHeaders.ETAG, response.getETag()); // <6>
     }
 
     private static String buildKey(String userId) {
         return userId + ".jpg";
     }
 
-    private URI location(String userId) {
-        String uriString = String.format("%s/%s", PREFIX, userId);
-        try {
-            return new URI(server.getScheme(), null, server.getHost(), server.getPort(), uriString, null, null);
-        } catch (URISyntaxException e) {
-            return URI.create(uriString);
-        }
+    private URI location(HttpRequest<?> request, String userId) {
+        return UriBuilder.of(httpHostResolver.resolve(request))
+                .path(PREFIX)
+                .path(userId)
+                .build();
     }
     //end::upload[]
-
 
     //tag::download[]
     @Override
@@ -79,8 +78,10 @@ public class ProfilePicturesController implements ProfilePicturesApi {
 
     private static HttpResponse<StreamedFile> buildStreamedFile(AwsS3ObjectStorageEntry entry) {
         GetObjectResponse nativeEntry = entry.getNativeEntry();
-        StreamedFile file = new StreamedFile(entry.getInputStream(), MediaType.of(nativeEntry.contentType())).attach(entry.getKey());
-        MutableHttpResponse<Object> httpResponse = HttpResponse.ok().header(ETAG_HEADER, nativeEntry.eTag()); // <3>
+        MediaType mediaType = MediaType.of(nativeEntry.contentType());
+        StreamedFile file = new StreamedFile(entry.getInputStream(), mediaType).attach(entry.getKey());
+        MutableHttpResponse<Object> httpResponse = HttpResponse.ok()
+                .header(HttpHeaders.ETAG, nativeEntry.eTag()); // <3>
         file.process(httpResponse);
         return httpResponse.body(file);
     }
@@ -90,7 +91,6 @@ public class ProfilePicturesController implements ProfilePicturesApi {
     @Override
     public void delete(String userId) {
         String key = buildKey(userId);
-
         objectStorage.delete(key);
     }
     //end::delete[]
