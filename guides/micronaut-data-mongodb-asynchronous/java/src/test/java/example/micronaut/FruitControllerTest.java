4e6ae5ba245a1813764969bda6e3c128a2a3fc2e
Sergio del Amo <sergio.delamo@softamo.com>
2022-09-20 11:14:12 +0200

    transactional falso for mongo async tests

--- guides/micronaut-data-mongodb-asynchronous/java/src/test/java/example/micronaut/FruitControllerTest.java
+++ guides/micronaut-data-mongodb-asynchronous/java/src/test/java/example/micronaut/FruitControllerTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@MicronautTest
+@MicronautTest(transactional = false) // <1>
 class FruitControllerTest {
 
     @Inject
