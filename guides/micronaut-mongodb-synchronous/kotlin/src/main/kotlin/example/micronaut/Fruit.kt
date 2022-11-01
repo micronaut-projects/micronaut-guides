c543386146842a67f51d98063b5edbded6f942c6
Tim Yates <tim.yates@gmail.com>
2022-11-01 15:31:16 +0000

    Switch to use Serde for the Mongo Sync guide (#1154)

--- guides/micronaut-mongodb-synchronous/kotlin/src/main/kotlin/example/micronaut/Fruit.kt
+++ guides/micronaut-mongodb-synchronous/kotlin/src/main/kotlin/example/micronaut/Fruit.kt
@@ -1,12 +1,12 @@
 package example.micronaut
 
 import io.micronaut.core.annotation.Creator
-import io.micronaut.core.annotation.Introspected
+import io.micronaut.serde.annotation.Serdeable
 import org.bson.codecs.pojo.annotations.BsonCreator
 import org.bson.codecs.pojo.annotations.BsonProperty
 import javax.validation.constraints.NotBlank
 
-@Introspected // <1>
+@Serdeable // <1>
 data class Fruit @Creator @BsonCreator constructor( // <4>
     @field:BsonProperty("name") @param:BsonProperty("name") @field:NotBlank val name: String, // <2> <3>
     @field:BsonProperty("description") @param:BsonProperty("description") var description: String?) { // <3>
