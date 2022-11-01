d496ebd1baba0a1e15a7f90c45fba433e3f9597f
Tim Yates <tim.yates@gmail.com>
2022-11-01 11:27:35 +0000

    Switch to use Serde for the Mongo Async guide

--- guides/micronaut-mongodb-asynchronous/java/src/main/java/example/micronaut/Fruit.java
+++ guides/micronaut-mongodb-asynchronous/java/src/main/java/example/micronaut/Fruit.java
@@ -1,15 +1,15 @@
 package example.micronaut;
 
 import io.micronaut.core.annotation.Creator;
-import io.micronaut.core.annotation.Introspected;
 import io.micronaut.core.annotation.NonNull;
 import io.micronaut.core.annotation.Nullable;
+import io.micronaut.serde.annotation.Serdeable;
 import org.bson.codecs.pojo.annotations.BsonCreator;
 import org.bson.codecs.pojo.annotations.BsonProperty;
 
 import javax.validation.constraints.NotBlank;
 
-@Introspected // <1>
+@Serdeable // <1>
 public class Fruit {
 
     @NonNull
