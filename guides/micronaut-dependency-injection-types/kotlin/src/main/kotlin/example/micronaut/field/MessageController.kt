9e5f90433f0e337d674292c078df709db6ecec9e
Tim Yates <tim.yates@gmail.com>
2023-01-16 11:16:41 +0000

    Remove unused semicolon

--- guides/micronaut-dependency-injection-types/kotlin/src/main/kotlin/example/micronaut/field/MessageController.kt
+++ guides/micronaut-dependency-injection-types/kotlin/src/main/kotlin/example/micronaut/field/MessageController.kt
@@ -5,7 +5,7 @@ import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Produces
-import jakarta.inject.Inject;
+import jakarta.inject.Inject
 
 @Controller("/field")
 class MessageController {
