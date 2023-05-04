32faf64f953c2de77b7c03f222d73374e3103266
Tim Yates <tim.yates@gmail.com>
2023-05-04 16:14:57 +0100

    Simplify TC for object-storage-gcp and fix for tc-cloud

--- guides/micronaut-object-storage-gcp/java/src/test/java/example/micronaut/FakeGcsServerContainer.java
+++ guides/micronaut-object-storage-gcp/java/src/test/java/example/micronaut/FakeGcsServerContainer.java
@@ -1,32 +1,22 @@
 package example.micronaut;
 
-import io.micronaut.core.io.socket.SocketUtils;
-import org.testcontainers.containers.FixedHostPortGenericContainer;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.utility.DockerImageName;
 
-public class FakeGcsServerContainer extends FixedHostPortGenericContainer<FakeGcsServerContainer> {
+class FakeGcsServerContainer extends GenericContainer<FakeGcsServerContainer> {
 
-    public static final String DEFAULT_IMAGE_NAME = "fsouza/fake-gcs-server:1.40.1";
-    public static final int DEFAULT_PORT = 4443;
+    private static final String DEFAULT_IMAGE_NAME = "fsouza/fake-gcs-server:1.40.1";
+    private static final int DEFAULT_PORT = 4443;
 
-    private int port;
-
-    public FakeGcsServerContainer(String dockerImageName) {
-        super(dockerImageName);
+    public FakeGcsServerContainer() {
+        super(DEFAULT_IMAGE_NAME);
+        this.withExposedPorts(DEFAULT_PORT)
+                .withCreateContainerCmdModifier(cmd -> cmd.withEntrypoint("/bin/fake-gcs-server", "-scheme", "http"));
     }
 
-    @Override
-    protected void configure() {
-        super.configure();
-
-        this.port = SocketUtils.findAvailableTcpPort();
-        addFixedExposedPort(port, DEFAULT_PORT);
-
-        withCreateContainerCmdModifier(cmd -> cmd.withEntrypoint("/bin/fake-gcs-server", "-scheme", "http"));
-    }
-
-    public int getPort() {
-        return port;
+    public String getUrl() {
+        if (!isRunning()) {
+            start();
+        }
+        return String.format("http://%s:%s", getHost(), getMappedPort(DEFAULT_PORT));
     }
 }
