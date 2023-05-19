c539a21a914f4707d60da1d85ce062d3f69c710e
Sergio del Amo <sergio.delamo@softamo.com>
2023-05-19 10:27:43 +0200

    Use ErrorResponseProcessor in ExceptionHandler

--- guides/micronaut-k8s/api/java/src/main/java/example/micronaut/ErrorExceptionHandler.java
+++ guides/micronaut-k8s/api/java/src/main/java/example/micronaut/ErrorExceptionHandler.java
@@ -4,15 +4,24 @@ import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import io.micronaut.http.server.exceptions.ExceptionHandler;
+import io.micronaut.http.server.exceptions.response.ErrorContext;
+import io.micronaut.http.server.exceptions.response.ErrorResponseProcessor;
 import jakarta.inject.Singleton;
 
 @Singleton // <1>
 class ErrorExceptionHandler implements ExceptionHandler<HttpClientResponseException, HttpResponse<?>> {
 
+    private final ErrorResponseProcessor<?> errorResponseProcessor;
+
+    public ErrorExceptionHandler(ErrorResponseProcessor<?> errorResponseProcessor) {
+        this.errorResponseProcessor = errorResponseProcessor;
+    }
+
     @Override
-    public HttpResponse<?> handle(HttpRequest request, HttpClientResponseException exception) {
-        return HttpResponse
-                .status(exception.getResponse().status())
-                .body(exception.getResponse().getBody(String.class).orElse(null));
+    public HttpResponse handle(HttpRequest request, HttpClientResponseException e) {
+        return errorResponseProcessor.processResponse(ErrorContext.builder(request)
+                .cause(e)
+                .errorMessage(e.getResponse().getBody(String.class).orElse(null))
+                .build(), HttpResponse.status(e.getStatus()));
     }
 }
