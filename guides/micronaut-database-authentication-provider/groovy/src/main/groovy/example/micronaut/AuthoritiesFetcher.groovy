c788fab8c8d274c45aacd231dfdaaa3b1a4b02f3
Sergio del Amo <sergio.delamo@softamo.com>
2023-06-30 12:51:53 +0200

    delete GORM guides

--- guides/micronaut-database-authentication-provider/groovy/src/main/groovy/example/micronaut/AuthoritiesFetcher.groovy
+++
@@ -1,5 +0,0 @@
-package example.micronaut
-
-interface AuthoritiesFetcher {
-    List<String> findAuthoritiesByUsername(String username)
-}
--- guides/micronaut-database-authentication-provider/groovy/src/main/groovy/example/micronaut/UserFetcher.groovy
+++
@@ -1,9 +0,0 @@
-package example.micronaut
-
-import io.micronaut.core.annotation.NonNull
-
-import jakarta.validation.constraints.NotBlank
-
-interface UserFetcher {
-    UserState findByUsername(@NotBlank @NonNull String username)
-}
