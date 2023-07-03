32444f4007bbb007db81879e6c4f67567ffa5998
Tim Yates <tim.yates@gmail.com>
2023-07-03 09:14:55 +0100

    Fix micronaut-security-basicauth

--- guides/micronaut-security-basicauth/java/src/main/java/example/micronaut/DefaultAuthorizationExceptionHandlerReplacement.java
+++ guides/micronaut-security-basicauth/java/src/main/java/example/micronaut/DefaultAuthorizationExceptionHandlerReplacement.java
@@ -1,11 +1,16 @@
 package example.micronaut;
 
 import io.micronaut.context.annotation.Replaces;
+import io.micronaut.core.annotation.Nullable;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MutableHttpResponse;
+import io.micronaut.http.server.exceptions.response.ErrorResponseProcessor;
 import io.micronaut.security.authentication.AuthorizationException;
 import io.micronaut.security.authentication.DefaultAuthorizationExceptionHandler;
+import io.micronaut.security.config.RedirectConfiguration;
+import io.micronaut.security.config.RedirectService;
+import io.micronaut.security.errors.PriorToLoginPersistence;
 import jakarta.inject.Singleton;
 
 import static io.micronaut.http.HttpHeaders.WWW_AUTHENTICATE;
@@ -15,6 +20,15 @@ import static io.micronaut.http.HttpStatus.UNAUTHORIZED;
 @Singleton // <1>
 @Replaces(DefaultAuthorizationExceptionHandler.class) // <2>
 public class DefaultAuthorizationExceptionHandlerReplacement extends DefaultAuthorizationExceptionHandler {
+
+    public DefaultAuthorizationExceptionHandlerReplacement(
+            ErrorResponseProcessor<?> errorResponseProcessor,
+            RedirectConfiguration redirectConfiguration,
+            RedirectService redirectService,
+            @Nullable PriorToLoginPersistence priorToLoginPersistence
+    ) {
+        super(errorResponseProcessor, redirectConfiguration, redirectService, priorToLoginPersistence);
+    }
 
     @Override
     protected MutableHttpResponse<?> httpResponseWithStatus(HttpRequest request,
--- guides/micronaut-security-basicauth/kotlin/src/main/kotlin/example/micronaut/DefaultAuthorizationExceptionHandlerReplacement.kt
+++ guides/micronaut-security-basicauth/kotlin/src/main/kotlin/example/micronaut/DefaultAuthorizationExceptionHandlerReplacement.kt
@@ -7,20 +7,33 @@ import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus.FORBIDDEN
 import io.micronaut.http.HttpStatus.UNAUTHORIZED
 import io.micronaut.http.MutableHttpResponse
+import io.micronaut.http.server.exceptions.response.ErrorResponseProcessor
 import io.micronaut.security.authentication.AuthorizationException
 import io.micronaut.security.authentication.DefaultAuthorizationExceptionHandler
+import io.micronaut.security.config.RedirectConfiguration
+import io.micronaut.security.config.RedirectService
+import io.micronaut.security.errors.PriorToLoginPersistence
 import jakarta.inject.Singleton
 
 @Singleton // <1>
 @Replaces(DefaultAuthorizationExceptionHandler::class) // <2>
-class DefaultAuthorizationExceptionHandlerReplacement : DefaultAuthorizationExceptionHandler() {
+class DefaultAuthorizationExceptionHandlerReplacement(
+    errorResponseProcessor: ErrorResponseProcessor<*>,
+    redirectConfiguration: RedirectConfiguration,
+    redirectService: RedirectService,
+    priorToLoginPersistence: PriorToLoginPersistence<*, *>?
+) : DefaultAuthorizationExceptionHandler(
+    errorResponseProcessor, redirectConfiguration, redirectService, priorToLoginPersistence
+) {
 
-    override fun httpResponseWithStatus(request: HttpRequest<*>,
-                                        e: AuthorizationException): MutableHttpResponse<*> =
-            if (e.isForbidden == true) {
-                HttpResponse.status<Any>(FORBIDDEN);
-            } else {
-                HttpResponse.status<Any>(UNAUTHORIZED)
-                        .header(WWW_AUTHENTICATE, "Basic realm=\"Micronaut Guide\"")
-            }
+    override fun httpResponseWithStatus(
+        request: HttpRequest<*>,
+        e: AuthorizationException
+    ): MutableHttpResponse<*> =
+        if (e.isForbidden) {
+            HttpResponse.status<Any>(FORBIDDEN);
+        } else {
+            HttpResponse.status<Any>(UNAUTHORIZED)
+                .header(WWW_AUTHENTICATE, "Basic realm=\"Micronaut Guide\"")
+        }
 }
