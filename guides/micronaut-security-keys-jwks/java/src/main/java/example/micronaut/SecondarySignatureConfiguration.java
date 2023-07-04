926bd47f52f15efeea5a2bcac5ee424ad2a10217
Sergio del Amo <sergio.delamo@softamo.com>
2023-07-04 14:11:35 +0200

    Use Name qualifier
    https://github.com/micronaut-projects/micronaut-core/pull/9534

--- guides/micronaut-security-keys-jwks/java/src/main/java/example/micronaut/SecondarySignatureConfiguration.java
+++ guides/micronaut-security-keys-jwks/java/src/main/java/example/micronaut/SecondarySignatureConfiguration.java
@@ -1,7 +1,9 @@
 package example.micronaut;
 
 import io.micronaut.runtime.context.scope.Refreshable;
+import jakarta.inject.Named;
 
+@Named("secondary")
 @Refreshable // <1>
 public class SecondarySignatureConfiguration extends AbstractRSASignatureConfiguration {
 
--- guides/micronaut-security-keys-jwks/kotlin/src/main/kotlin/example/micronaut/SecondarySignatureConfiguration.kt
+++ guides/micronaut-security-keys-jwks/kotlin/src/main/kotlin/example/micronaut/SecondarySignatureConfiguration.kt
@@ -1,7 +1,8 @@
 package example.micronaut
 
 import io.micronaut.runtime.context.scope.Refreshable
-
+import jakarta.inject.Named
 @Refreshable // <1>
+@Named("secondary")
 class SecondarySignatureConfiguration(jwkConfiguration: JwkConfiguration) :
         AbstractRSASignatureConfiguration(jwkConfiguration.secondary)
