c07d8c51cf24d684f23ba9acbade9bc932c7073c
sdelamo <sergio.delamo@softamo.com>
2023-07-22 10:51:59 +0200

    Consuming a REST API [ci skip]

---
+++ guides/spring-boot-to-micronaut-consuming-rest/springboot/java/src/main/java/example/micronaut/Application.java
@@ -0,0 +1,34 @@
+package example.micronaut;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject(
+                    "http://localhost:8080/api/random", Quote.class);
+            log.info(quote.toString());
+        };
+    }
+}
