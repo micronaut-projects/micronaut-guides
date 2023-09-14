ae605b03eaf5f5f9d56faaf8940e55850e307d58
Tim Yates <tim.yates@gmail.com>
2023-09-14 13:57:09 +0100

    Remove unused import and fix whitespace

--- guides/testing-rest-api-integrations-using-mockserver/java/src/main/java/example/micronaut/AlbumController.java
+++ guides/testing-rest-api-integrations-using-mockserver/java/src/main/java/example/micronaut/AlbumController.java
@@ -1,14 +1,15 @@
 package example.micronaut;
 
-import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.scheduling.annotation.ExecuteOn;
 
 import static io.micronaut.scheduling.TaskExecutors.BLOCKING;
+
 @Controller("/api") // <1>
 class AlbumController {
+
     private final PhotoServiceClient photoServiceClient;
 
     AlbumController(PhotoServiceClient photoServiceClient) { // <2>
