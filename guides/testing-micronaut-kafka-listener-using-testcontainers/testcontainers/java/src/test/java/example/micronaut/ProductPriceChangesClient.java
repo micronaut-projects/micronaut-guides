376b96b6b552fc4251a205be427c093d801aeb6f
Sergio del Amo <sergio.delamo@softamo.com>
2023-09-15 11:41:06 +0200

    testing-micronaut-kafka-listener-using-testcontainers
    Port of Spring Boot Guide https://testcontainers.com/guides/testing-spring-boot-kafka-listener-using-testcontainers
    
    I added a Test Resources section to show how you can simplify testing.
    
    To generate guide locally:
    
    ./gradlew testingMicronautKafkaListenerUsingTestcontainersBuild
    
    Open HTML locally:
    
    open build/dist/index.html
    
    Code available:
    
    build/code/testing-micronaut-kafka-listener-using-testcontainers

---
+++ guides/testing-micronaut-kafka-listener-using-testcontainers/testcontainers/java/src/test/java/example/micronaut/ProductPriceChangesClient.java
@@ -0,0 +1,12 @@
+package example.micronaut;
+
+import io.micronaut.configuration.kafka.annotation.KafkaClient;
+import io.micronaut.configuration.kafka.annotation.KafkaKey;
+import io.micronaut.configuration.kafka.annotation.Topic;
+
+@KafkaClient // <1>
+public interface ProductPriceChangesClient {
+    @Topic("product-price-changes") // <2>
+    public void send(@KafkaKey String productCode, // <3>
+                     ProductPriceChangedEvent event);
+}
---
+++ guides/testing-micronaut-kafka-listener-using-testcontainers/testresources/java/src/main/java/example/micronaut/ProductRepository.java
@@ -0,0 +1,15 @@
+package example.micronaut;
+
+import io.micronaut.data.annotation.Query;
+import io.micronaut.data.annotation.Repository;
+import io.micronaut.data.jpa.repository.JpaRepository;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@Repository // <1>
+public interface ProductRepository extends JpaRepository<Product, Long> { // <2>
+  Optional<Product> findByCode(String code);
+  @Query("update Product p set p.price = :price where p.code = :productCode") // <3>
+  void updateProductPrice(String productCode, BigDecimal price);
+}
