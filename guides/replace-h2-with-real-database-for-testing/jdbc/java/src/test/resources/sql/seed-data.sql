015ee15cbc1ff1f054c2197e6706c73bd8fc9986
Sergio del Amo <sergio.delamo@softamo.com>
2023-10-03 16:50:43 +0200

    share seed-data.sql

--- guides/replace-h2-with-real-database-for-testing/jdbc/java/src/test/resources/sql/seed-data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO products(id, code, name) VALUES(1, 'p101', 'Apple MacBook Pro') ON CONFLICT DO NOTHING;
-INSERT INTO products(id, code, name) VALUES(2, 'p102', 'Sony TV') ON CONFLICT DO NOTHING;
--- guides/replace-h2-with-real-database-for-testing/postgresql/java/src/test/resources/sql/seed-data.sql
+++
@@ -1,2 +0,0 @@
-insert into products(id, code, name) values(1, 'p101', 'Apple MacBook Pro') ON CONFLICT DO NOTHING;
-insert into products(id, code, name) values(2, 'p102', 'Sony TV') ON CONFLICT DO NOTHING;
