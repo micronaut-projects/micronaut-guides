b278fdf11f2d07059a40ce0f8b7d54cd6615a1fa
Tim Yates <tim.yates@gmail.com>
2023-11-02 13:33:47 +0000

    Add Nullable to QueryValues in micronaut-graalvm-reflection

--- guides/micronaut-graalvm-reflection/reflectiveaccess/java/src/main/java/example/micronaut/StringTransformerController.java
+++ guides/micronaut-graalvm-reflection/reflectiveaccess/java/src/main/java/example/micronaut/StringTransformerController.java
@@ -1,5 +1,6 @@
 package example.micronaut;
 
+import io.micronaut.core.annotation.Nullable;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
@@ -17,13 +18,13 @@ public class StringTransformerController {
 
     @Produces(MediaType.TEXT_PLAIN) // <3>
     @Get("/capitalize{?q}") // <4>
-    String capitalize(@QueryValue String q) { // <5>
+    String capitalize(@Nullable @QueryValue String q) { // <5>
         return transformer.transform(q, "example.micronaut.StringCapitalizer", "capitalize");
     }
 
     @Produces(MediaType.TEXT_PLAIN) // <3>
     @Get("/reverse{?q}") // <4>
-    String reverse(@QueryValue String q) { // <5>
+    String reverse(@Nullable @QueryValue String q) { // <5>
         return transformer.transform(q, "example.micronaut.StringReverser", "reverse");
     }
 }
