21bc5188bd3d8a2c2b3492f5fb925d6ff5b6dada
Sergio del Amo <sergio.delamo@softamo.com>
2023-11-17 08:37:22 +0100

    Update to 4.2.0 (#1370)
    * 4.2.0-SNAPSHOT
    
    * don’t specify all-open version
    
    * oracle autonomous db
    
    * Replace MySQL feature to use old dependency
    
    Without this, the Maven/TestResources integration doesn't work as it's using an old version that doesn't know about connector-j
    
    * Delete buildSrc/src/main/java/io/micronaut/guides/feature/FlywayReplacement.java
    
    * Delete buildSrc/src/main/java/io/micronaut/guides/feature/database/MySQLReplacement.java
    
    ---------
    
    Co-authored-by: Tim Yates <tim.yates@gmail.com>

--- guides/micronaut-oracle-autonomous-db/java/src/test/java/example/micronaut/repository/ThingRepositoryTest.java
+++ guides/micronaut-oracle-autonomous-db/java/src/test/java/example/micronaut/repository/ThingRepositoryTest.java
@@ -1,28 +1,40 @@
 package example.micronaut.repository;
 
+import com.github.dockerjava.api.model.Info;
 import example.micronaut.domain.Thing;
-import example.micronaut.repository.ThingRepository;
-import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.micronaut.context.ApplicationContext;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIf;
+import org.testcontainers.DockerClientFactory;
 
-import jakarta.inject.Inject;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
-@MicronautTest
 class ThingRepositoryTest {
 
-    @Inject
-    ThingRepository thingRepository;
+    /**
+     * WARN  t.gvenzl/oracle-xe:21-slim-faststart - The architecture 'amd64' for image 'gvenzl/oracle-xe:21-slim-faststart'
+     * (ID sha256:395e7780aaba5f8c33082bf533a17a4bffdb7bcdd58034702a1634fcbd3d1137) does not match the Docker server architecture 'arm64'.
+     * This will cause the container to execute much more slowly due to emulation and may lead to timeout failures.
+     */
+    boolean dockerArchitecture() {
+        Info info = DockerClientFactory.instance().getInfo();
+        String architecture = info.getArchitecture();
+        if (architecture == null) {
+            return true;
+        }
+        return architecture.equals("x86_64");
+    }
 
+    @EnabledIf("dockerArchitecture")
     @Test
     void testFindAll() {
+        ApplicationContext applicationContext = ApplicationContext.run(Oracle.getConfiguration());
+        ThingRepository thingRepository = applicationContext.getBean(ThingRepository.class);
 
         // clear out existing data; safe because each
         // test runs in a transaction that's rolled back
@@ -42,10 +54,15 @@ class ThingRepositoryTest {
                         .map(Thing::getName)
                         .sorted()
                         .collect(Collectors.toList()));
+        applicationContext.close();
     }
 
+    @EnabledIf("dockerArchitecture")
     @Test
     void testFindByName() {
+        ApplicationContext applicationContext = ApplicationContext.run(Oracle.getConfiguration());
+        ThingRepository thingRepository = applicationContext.getBean(ThingRepository.class);
+
         String name = UUID.randomUUID().toString();
 
         Thing thing = thingRepository.findByName(name).orElse(null);
@@ -55,5 +72,6 @@ class ThingRepositoryTest {
         thing = thingRepository.findByName(name).orElse(null);
         assertNotNull(thing);
         assertEquals(name, thing.getName());
+        applicationContext.close();
     }
 }
