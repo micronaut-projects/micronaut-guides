b0c18c0985781b00f9de9444603c5381492315eb
Sergio del Amo <sergio.delamo@softamo.com>
2023-11-17 08:37:22 +0100

    Update to 4.2.0 (#1370)
    * 4.2.0-SNAPSHOT
    
    * don’t specify all-open version
    
    * oracle autonomous db
    
    * Replace MySQL feature to use old dependency
    
    Without this, the Maven/TestResources integration doesn't work as it's using an old version that doesn't know about connector-j
    
    * Delete buildSrc/src/main/java/io/micronaut/guides/feature/FlywayReplacement.java
    
    * Delete buildSrc/src/main/java/io/micronaut/guides/feature/database/MySQLReplacement.java
    
    ---------
    
    Co-authored-by: Tim Yates <tim.yates@gmail.com>

--- guides/micronaut-oracle-autonomous-db/groovy/src/test/groovy/example/micronaut/repository/ThingRepositorySpec.groovy
+++ guides/micronaut-oracle-autonomous-db/groovy/src/test/groovy/example/micronaut/repository/ThingRepositorySpec.groovy
@@ -1,61 +1,80 @@
 package example.micronaut.repository
 
+import com.github.dockerjava.api.model.Info
 import example.micronaut.domain.Thing
-import example.micronaut.repository.ThingRepository
-import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import io.micronaut.context.ApplicationContext
+import spock.lang.IgnoreIf
+import org.testcontainers.DockerClientFactory
 import spock.lang.Specification
 
-import jakarta.inject.Inject
-import java.util.stream.Collectors
-
-@MicronautTest
 class ThingRepositorySpec extends Specification {
 
-    @Inject
-    ThingRepository thingRepository
+    /**
+     * WARN  t.gvenzl/oracle-xe:21-slim-faststart - The architecture 'amd64' for image 'gvenzl/oracle-xe:21-slim-faststart'
+     * (ID sha256:395e7780aaba5f8c33082bf533a17a4bffdb7bcdd58034702a1634fcbd3d1137) does not match the Docker server architecture 'arm64'.
+     * This will cause the container to execute much more slowly due to emulation and may lead to timeout failures.
+     */
+    static boolean dockerArchitecture() {
+        Info info = DockerClientFactory.instance().getInfo()
+        String architecture = info.getArchitecture()
+        if (!architecture) {
+            return true
+        }
+        architecture == "x86_64"
+    }
 
-    void 'test findAll'() {
+    @IgnoreIf({ !dockerArchitecture() })
+    void testFindAll() {
+        given:
+        ApplicationContext applicationContext = ApplicationContext.run(Oracle.getConfiguration())
+        ThingRepository thingRepository = applicationContext.getBean(ThingRepository.class)
 
-        when:
         // clear out existing data; safe because each
         // test runs in a transaction that's rolled back
+        when:
         thingRepository.deleteAll()
 
         then:
-        !thingRepository.count()
+        0 == thingRepository.count()
 
         when:
         thingRepository.saveAll(Arrays.asList(
-                new Thing('t1'),
-                new Thing('t2'),
-                new Thing('t3')))
-
+                new Thing("t1"),
+                new Thing("t2"),
+                new Thing("t3")))
         List<Thing> things = thingRepository.findAll()
 
         then:
-        things.size() == 3
-        ['t1', 't2', 't3'] == things.stream()
-                .map(Thing::getName)
-                .sorted()
-                .collect(Collectors.toList())
+        3 == things.size()
+        Arrays.asList("t1", "t2", "t3") ==
+                things.stream()
+                        .map(Thing::getName)
+                        .sorted()
+                        .toList()
+        cleanup:
+        applicationContext.close()
     }
 
-    void 'test findByName'() {
+    @IgnoreIf({ !dockerArchitecture() })
+    void testFindByName() {
         given:
-        String name = UUID.randomUUID()
-
+        ApplicationContext applicationContext = ApplicationContext.run(Oracle.getConfiguration())
+        ThingRepository thingRepository = applicationContext.getBean(ThingRepository.class)
+        String name = UUID.randomUUID().toString()
         when:
-        Thing thing = thingRepository.findByName(name).orElse(null)
-
+        Thing thing = thingRepository.findByName(name).orElse(null);
         then:
         !thing
 
         when:
-        thingRepository.save(new Thing(name))
-        thing = thingRepository.findByName(name).orElse(null)
+        thingRepository.save(new Thing(name));
+        thing = thingRepository.findByName(name).orElse(null);
 
         then:
         thing
         name == thing.name
+
+        cleanup:
+        applicationContext.close()
     }
 }
--- guides/micronaut-security-basicauth/groovy/src/main/groovy/example/micronaut/AuthenticationProviderUserPassword.groovy
+++ guides/micronaut-security-basicauth/groovy/src/main/groovy/example/micronaut/AuthenticationProviderUserPassword.groovy
@@ -1,6 +1,5 @@
 package example.micronaut
 
-import groovy.transform.CompileStatic
 import io.micronaut.core.annotation.Nullable
 import io.micronaut.http.HttpRequest
 import io.micronaut.security.authentication.AuthenticationProvider
@@ -11,21 +10,19 @@ import org.reactivestreams.Publisher
 import reactor.core.publisher.Flux
 import reactor.core.publisher.FluxSink
 
-@CompileStatic
 @Singleton // <1>
 class AuthenticationProviderUserPassword implements AuthenticationProvider<HttpRequest<?>> { // <2>
 
     @Override
     Publisher<AuthenticationResponse> authenticate(@Nullable HttpRequest<?> httpRequest,
                                                    AuthenticationRequest<?, ?> authenticationRequest) {
-        Flux.create({ emitter ->
-            if ( authenticationRequest.identity == "sherlock" && authenticationRequest.secret == "password" ) {
+        Flux.create(emitter -> {
+            if (authenticationRequest.identity == "sherlock" && authenticationRequest.secret == "password") {
                 emitter.next(AuthenticationResponse.success((String) authenticationRequest.identity))
                 emitter.complete()
             } else {
                 emitter.error(AuthenticationResponse.exception())
             }
-
         }, FluxSink.OverflowStrategy.ERROR)
     }
 }
