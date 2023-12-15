a97b968ba9829f2cbaaa54405ed3fc6b1069454e
Sergio del Amo <sergio.delamo@softamo.com>
2023-12-15 19:45:39 +0100

    ReactorStreamingHttpClient and StreamedFile Guide (#1397)
    * ReactorStreamingHttpClient Guide and StreamedFile
    
    ./gradlew micronautStreamedFileAndReactorStreamingHttpClientBuild
    
    The app works in a GraalVM Native Image but the test fails so I ignored in nativeTest
    
    * Update guides/micronaut-streamed-file-and-reactor-streaming-http-client/java/src/main/java/example/micronaut/HomeController.java
    
    Co-authored-by: Jonas Konrad <me@yawk.at>
    
    * use image in gh-pages
    
    * Whitespace, field order, unused imports and HexFormat 😎
    
    * Reuse HexFormat and remove undocumented <3>
    
    ---------
    
    Co-authored-by: Jonas Konrad <me@yawk.at>
    Co-authored-by: Tim Yates <tim.yates@gmail.com>

---
+++ guides/micronaut-streamed-file-and-reactor-streaming-http-client/java/src/test/java/example/micronaut/HomeControllerTest.java
@@ -0,0 +1,53 @@
+package example.micronaut;
+
+import io.micronaut.core.io.ResourceLoader;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.client.BlockingHttpClient;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledInNativeImage;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@MicronautTest // <1>
+class HomeControllerTest {
+
+    private final HexFormat hexFormat = HexFormat.of();
+
+    @DisabledInNativeImage
+    @Test
+    void downloadFile(@Client("/") HttpClient httpClient, // <2>
+                      ResourceLoader resourceLoader)
+            throws URISyntaxException, IOException, NoSuchAlgorithmException {
+        Optional<URL> resource = resourceLoader.getResource("micronaut5K.png");
+        assertTrue(resource.isPresent());
+        byte[] expectedByteArray = Files.readAllBytes(Path.of(resource.get().toURI()));
+
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] expectedEncodedHash = digest.digest(expectedByteArray);
+        String expected = hexFormat.formatHex(expectedEncodedHash);
+
+        BlockingHttpClient client = httpClient.toBlocking();
+        HttpResponse<byte[]> resp = assertDoesNotThrow(() -> client.exchange(HttpRequest.GET("/"), byte[].class));
+        byte[] responseBytes = resp.body();
+
+        byte[] responseEncodedHash = digest.digest(responseBytes);
+        String response = hexFormat.formatHex(responseEncodedHash);
+        assertEquals(expected, response);
+    }
+}
