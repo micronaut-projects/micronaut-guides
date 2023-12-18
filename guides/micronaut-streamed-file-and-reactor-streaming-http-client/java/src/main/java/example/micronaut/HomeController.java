a076076507798cd04627b3fc860df0e067f07892
Sergio del Amo <sergio.delamo@softamo.com>
2023-12-18 11:21:37 +0100

    Remove StreamedFile mention

--- guides/micronaut-streamed-file-and-reactor-streaming-http-client/java/src/main/java/example/micronaut/HomeController.java
+++ guides/micronaut-streamed-file-and-reactor-streaming-http-client/java/src/main/java/example/micronaut/HomeController.java
@@ -1,28 +1,22 @@
 package example.micronaut;
 
 import io.micronaut.context.exceptions.ConfigurationException;
-import io.micronaut.core.annotation.Nullable;
+import io.micronaut.core.io.buffer.ByteBuffer;
 import io.micronaut.http.HttpRequest;
-import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.QueryValue;
-import io.micronaut.http.server.types.files.StreamedFile;
 import io.micronaut.reactor.http.client.ReactorStreamingHttpClient;
 import jakarta.annotation.PreDestroy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
 
-import java.io.IOException;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 
 @Controller // <1>
 class HomeController implements AutoCloseable {
-
     private static final Logger LOG = LoggerFactory.getLogger(HomeController.class);
     private static final URI DEFAULT_URI = URI.create("https://guides.micronaut.io/micronaut5K.png");
 
@@ -39,44 +33,10 @@ class HomeController implements AutoCloseable {
         this.reactorStreamingHttpClient = ReactorStreamingHttpClient.create(url); // <2>
     }
 
-    @Get("{?q}") // <3>
-    StreamedFile download(@Nullable @QueryValue String q) throws IOException { // <4>
-        URI uri = q == null ? DEFAULT_URI : URI.create(q);
-        HttpRequest<?> request = HttpRequest.GET(uri);
-
-        LOG.trace("downloading: {}", uri);
-        PipedOutputStream outputStream = new PipedOutputStream();
-        dataStreamToOutputStream(request, outputStream, () -> LOG.trace("finished download"));
-
-        PipedInputStream inputStream = new PipedInputStream(1024*10);
-        inputStream.connect(outputStream);
-
-        return new StreamedFile(inputStream, MediaType.IMAGE_PNG_TYPE);
-    }
-
-    private void dataStreamToOutputStream(HttpRequest<?> request,
-                                          PipedOutputStream outputStream,
-                                          Runnable finallyRunnable) {
-        reactorStreamingHttpClient.dataStream(request) // <5>
-                .doOnNext(byteBuffer -> {
-                    LOG.trace("Saving byte array");
-                    try {
-                        outputStream.write(byteBuffer.toByteArray());
-                    } catch (IOException e) {
-                        LOG.error("IO Exception closing the stream", e);
-                    }
-                })
-                .doFinally(signalType -> {
-                    try {
-                        LOG.trace("Closing OutputStream");
-                        outputStream.close();
-                    } catch (IOException e) {
-                        LOG.error("IO Exception closing the stream", e);
-                        System.out.println();
-                    }
-                    finallyRunnable.run();
-                })
-                .subscribe();
+    @Get // <3>
+    Flux<ByteBuffer<?>> download() {
+        HttpRequest<?> request = HttpRequest.GET(DEFAULT_URI);
+        return reactorStreamingHttpClient.dataStream(request);  // <4>
     }
 
     @PreDestroy // <6>
