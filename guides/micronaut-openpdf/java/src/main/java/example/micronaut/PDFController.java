1395235006afaefbd60824dde5fc4405c2671928
Tim Yates <tim.yates@gmail.com>
2024-01-03 16:38:58 +0000

    Whitespace license and unused imports

--- guides/micronaut-openpdf/java/src/main/java/example/micronaut/PDFController.java
+++ guides/micronaut-openpdf/java/src/main/java/example/micronaut/PDFController.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package example.micronaut;
 
 import io.micronaut.http.HttpHeaders;
@@ -5,7 +20,6 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Produces;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
 
@@ -19,6 +33,7 @@ import com.lowagie.text.pdf.PdfWriter;
 
 @Controller("/pdf") // <1>
 class PDFController {
+
     @ExecuteOn(TaskExecutors.BLOCKING) // <2>
     @Get("/download") // <3>
     HttpResponse<byte[]> download() throws IOException {
--- guides/micronaut-openpdf/java/src/test/java/example/micronaut/PDFControllerTest.java
+++ guides/micronaut-openpdf/java/src/test/java/example/micronaut/PDFControllerTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package example.micronaut;
 
 import com.lowagie.text.pdf.PdfReader;
@@ -14,10 +29,12 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest // <1>
 class PDFControllerTest {
+
     @Test
     void testDownload(@Client("/") HttpClient httpClient) throws IOException { // <2>
         BlockingHttpClient client = httpClient.toBlocking();
@@ -32,7 +49,7 @@ class PDFControllerTest {
 
     private static String textAtPage(PdfReader pdfReader, int pageNumber) throws IOException {
         PdfTextExtractor pdfTextExtractor = new PdfTextExtractor(pdfReader);
-        return pdfTextExtractor.getTextFromPage(1);
+        return pdfTextExtractor.getTextFromPage(pageNumber);
     }
 
     private static String textAtPage(byte[] pdfBytes, int pageNumber) throws IOException {
