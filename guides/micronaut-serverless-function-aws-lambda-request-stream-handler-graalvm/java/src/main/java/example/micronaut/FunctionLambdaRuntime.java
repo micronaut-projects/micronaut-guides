19b91f5c8b7d4586deb9046d9ba8a91a8367b9f4
sdelamo <sergio.delamo@softamo.com>
2024-01-12 07:52:12 +0100

    add graalvm guide

---
+++ guides/micronaut-serverless-function-aws-lambda-request-stream-handler-graalvm/java/src/main/java/example/micronaut/FunctionLambdaRuntime.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2017-2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.micronaut;
+
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.function.aws.runtime.AbstractRequestStreamHandlerMicronautLambdaRuntime;
+
+import java.net.MalformedURLException;
+
+class FunctionLambdaRuntime extends AbstractRequestStreamHandlerMicronautLambdaRuntime<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    public static void main(String[] args) {
+        try {
+            new FunctionLambdaRuntime().run(args);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected @Nullable RequestStreamHandler createRequestStreamHandler(String... args) {
+        return new FunctionRequestHandler();
+    }
+}
