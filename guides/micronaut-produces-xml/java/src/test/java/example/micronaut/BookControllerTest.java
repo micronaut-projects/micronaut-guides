84421c568acf404ceaf27242c1e08649fae25302
Tim Yates <tim.yates@gmail.com>
2024-01-15 14:10:54 +0000

    Whitespace, license and imports

--- guides/micronaut-produces-xml/java/src/test/java/example/micronaut/BookControllerTest.java
+++ guides/micronaut-produces-xml/java/src/test/java/example/micronaut/BookControllerTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package example.micronaut;
 
 import io.micronaut.http.HttpRequest;
@@ -8,10 +23,12 @@ import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @MicronautTest // <1>
 class BookControllerTest {
+
     @Test
     void testXmlRendered(@Client("/") HttpClient httpClient) { // <2>
         BlockingHttpClient client = httpClient.toBlocking();
