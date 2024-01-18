117e848840325ee93045f19a1e5bba78335dd4a2
Sergio del Amo <sergio.delamo@softamo.com>
2024-01-18 11:43:07 +0100

    Revert "enable sqs guide"
    This reverts commit 2bcbb7e39047cb63c745b3ecf26dc63489e51648.

---
+++ guides/micronaut-jms-aws-sqs/groovy/src/test/groovy/example/micronaut/MicronautguideSpec.groovy
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2017-2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.micronaut
+
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import io.micronaut.test.support.TestPropertyProvider
+import jakarta.inject.Inject
+import spock.lang.Specification
+import spock.util.concurrent.PollingConditions
+
+@MicronautTest
+class MicronautguideSpec extends Specification implements TestPropertyProvider {
+
+    @Inject
+    @Client("/")
+    HttpClient client
+
+    @Inject
+    DemoConsumer demoConsumer
+
+    void 'test it works'() {
+        when:
+        int messageCount = demoConsumer.getMessageCount()
+
+        then:
+        messageCount == 0
+
+        when:
+        HttpResponse<?> response = client.toBlocking().exchange(HttpRequest.POST('/demo', [:]))
+
+        then:
+        new PollingConditions(initialDelay: 3, timeout: 10).eventually {
+            demoConsumer.getMessageCount() == 1
+        }
+    }
+
+    def cleanupSpec() {
+        LocalStack.close()
+    }
+
+    @Override
+    Map<String, String> getProperties() {
+        LocalStack.getProperties()
+    }
+}
---
+++ guides/micronaut-jms-aws-sqs/kotlin/src/test/kotlin/example/micronaut/MicronautguideTest.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2017-2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.micronaut
+
+import io.micronaut.core.annotation.NonNull
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import io.micronaut.test.support.TestPropertyProvider
+import jakarta.inject.Inject
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.testcontainers.junit.jupiter.Testcontainers
+import java.util.*
+
+@Testcontainers(disabledWithoutDocker = true)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@MicronautTest
+internal class MicronautguideTest : TestPropertyProvider {
+
+    @Inject
+    @field:Client("/")
+    lateinit var client : HttpClient
+
+    @Inject
+    lateinit var demoConsumer: DemoConsumer
+
+    @Test
+    fun testItWorks() {
+        var messageCount = demoConsumer.getMessageCount()
+        Assertions.assertTrue(messageCount == 0)
+
+        client.toBlocking().exchange<Any, Any>(HttpRequest.POST("/demo", emptyMap<Any, Any>()))
+        messageCount = demoConsumer.getMessageCount()
+        while (messageCount == 0) {
+            messageCount = demoConsumer.getMessageCount()
+        }
+        Assertions.assertTrue(messageCount == 1)
+    }
+
+    @NonNull
+    override fun getProperties(): Map<String, String> {
+        return LocalStackUtil.properties
+    }
+
+    @AfterAll
+    fun afterAll() {
+        LocalStackUtil.close()
+    }
+
+}
