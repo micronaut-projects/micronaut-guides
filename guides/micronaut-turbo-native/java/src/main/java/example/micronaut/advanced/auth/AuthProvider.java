44d547143616ff36cf9d92d8b71f82a20b39421c
Sergio del Amo <sergio.delamo@softamo.com>
2024-02-07 17:36:21 +0100

    micronaut-turbo-native

--- guides/micronaut-turbo-native/java/src/main/java/example/micronaut/advanced/auth/AuthProvider.java
+++ guides/micronaut-turbo-native/java/src/main/java/example/micronaut/advanced/auth/AuthProvider.java
@@ -15,20 +15,19 @@
  */
 package example.micronaut.advanced.auth;
 
+import io.micronaut.core.annotation.NonNull;
 import io.micronaut.core.annotation.Nullable;
 import io.micronaut.http.HttpRequest;
-import io.micronaut.security.authentication.AuthenticationProvider;
+import io.micronaut.security.authentication.AuthenticationFailureReason;
 import io.micronaut.security.authentication.AuthenticationRequest;
 import io.micronaut.security.authentication.AuthenticationResponse;
+import io.micronaut.security.authentication.provider.HttpRequestAuthenticationProvider;
 import jakarta.inject.Singleton;
-import org.reactivestreams.Publisher;
-import reactor.core.publisher.Mono;
 
-@Singleton
-class AuthProvider implements AuthenticationProvider<HttpRequest<?>> {
-
-    @Override
-    public Publisher<AuthenticationResponse> authenticate(@Nullable HttpRequest<?> httpRequest, AuthenticationRequest<?, ?> authenticationRequest) {
-        return Mono.create(it -> it.success(AuthenticationResponse.success(authenticationRequest.getIdentity().toString())));
+@Singleton // <1>
+class AuthProvider<B> implements HttpRequestAuthenticationProvider<B> { // <2>
+    public AuthenticationResponse authenticate(@Nullable HttpRequest<B> httpRequest,
+                                               @NonNull AuthenticationRequest<String, String> authenticationRequest) {
+        return AuthenticationResponse.success(authenticationRequest.getIdentity());
     }
 }
