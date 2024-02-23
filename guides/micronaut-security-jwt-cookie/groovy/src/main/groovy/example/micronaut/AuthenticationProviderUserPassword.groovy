15cb75e6c6360c8d18671efd69f1d7f868b6b776
Tim Yates <tim.yates@gmail.com>
2024-02-23 10:01:24 +0000

    Fix micronaut-security-jwt-cookie

--- guides/micronaut-security-jwt-cookie/groovy/src/main/groovy/example/micronaut/AuthenticationProviderUserPassword.groovy
+++ guides/micronaut-security-jwt-cookie/groovy/src/main/groovy/example/micronaut/AuthenticationProviderUserPassword.groovy
@@ -17,20 +17,20 @@ package example.micronaut
 
 import io.micronaut.core.annotation.Nullable
 import io.micronaut.http.HttpRequest
-import io.micronaut.security.authentication.AuthenticationProvider
 import io.micronaut.security.authentication.AuthenticationRequest
 import io.micronaut.security.authentication.AuthenticationResponse
+import io.micronaut.security.authentication.provider.HttpRequestReactiveAuthenticationProvider
 import jakarta.inject.Singleton
 import org.reactivestreams.Publisher
 import reactor.core.publisher.Flux
 import reactor.core.publisher.FluxSink
 
 @Singleton // <1>
-class AuthenticationProviderUserPassword implements AuthenticationProvider<HttpRequest<?>> { // <2>
+class AuthenticationProviderUserPassword<T> implements HttpRequestReactiveAuthenticationProvider<T> { // <2>
 
     @Override
-    Publisher<AuthenticationResponse> authenticate(@Nullable HttpRequest<?> httpRequest,
-                                                   AuthenticationRequest<?, ?> authenticationRequest) {
+    Publisher<AuthenticationResponse> authenticate(@Nullable HttpRequest<T> httpRequest,
+                                                   AuthenticationRequest<String, String> authenticationRequest) {
         Flux.create(emitter -> {
             if (authenticationRequest.identity == "sherlock" && authenticationRequest.secret == "password") {
                 emitter.next(AuthenticationResponse.success((String) authenticationRequest.identity))
--- guides/micronaut-security-jwt-cookie/kotlin/src/main/kotlin/example/micronaut/AuthenticationProviderUserPassword.kt
+++ guides/micronaut-security-jwt-cookie/kotlin/src/main/kotlin/example/micronaut/AuthenticationProviderUserPassword.kt
@@ -16,26 +16,26 @@
 package example.micronaut
 
 import io.micronaut.http.HttpRequest
-import io.micronaut.security.authentication.AuthenticationProvider
 import io.micronaut.security.authentication.AuthenticationRequest
 import io.micronaut.security.authentication.AuthenticationResponse
+import io.micronaut.security.authentication.provider.HttpRequestReactiveAuthenticationProvider
 import jakarta.inject.Singleton
 import org.reactivestreams.Publisher
 import reactor.core.publisher.Flux
 import reactor.core.publisher.FluxSink
 
 @Singleton // <1>
-class AuthenticationProviderUserPassword : AuthenticationProvider<HttpRequest<*>> { // <2>
+class AuthenticationProviderUserPassword<T> : HttpRequestReactiveAuthenticationProvider<T> { // <2>
 
-    override fun authenticate(httpRequest: HttpRequest<*>?,
-                              authenticationRequest: AuthenticationRequest<*, *>): Publisher<AuthenticationResponse> {
-        return Flux.create({ emitter: FluxSink<AuthenticationResponse> ->
-            if (authenticationRequest.identity == "sherlock" && authenticationRequest.secret == "password") {
-                emitter.next(AuthenticationResponse.success(authenticationRequest.identity as String))
-                emitter.complete()
-            } else {
-                emitter.error(AuthenticationResponse.exception())
-            }
-        }, FluxSink.OverflowStrategy.ERROR)
-    }
+    override fun authenticate(
+        httpRequest: HttpRequest<T>?,
+        authenticationRequest: AuthenticationRequest<String, String>
+    ) : Publisher<AuthenticationResponse> = Flux.create({ emitter: FluxSink<AuthenticationResponse> ->
+        if (authenticationRequest.identity == "sherlock" && authenticationRequest.secret == "password") {
+            emitter.next(AuthenticationResponse.success(authenticationRequest.identity as String))
+            emitter.complete()
+        } else {
+            emitter.error(AuthenticationResponse.exception())
+        }
+    }, FluxSink.OverflowStrategy.ERROR)
 }
