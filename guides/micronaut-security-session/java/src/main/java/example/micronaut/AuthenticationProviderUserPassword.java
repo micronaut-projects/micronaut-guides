89780a2311eef1732beca8c36e738ff794a73aae
Tim Yates <tim.yates@gmail.com>
2024-02-23 10:23:22 +0000

    Undeprecate micronaut-security-session

--- guides/micronaut-security-session/java/src/main/java/example/micronaut/AuthenticationProviderUserPassword.java
+++ guides/micronaut-security-session/java/src/main/java/example/micronaut/AuthenticationProviderUserPassword.java
@@ -15,22 +15,25 @@
  */
 package example.micronaut;
 
+import io.micronaut.core.annotation.NonNull;
 import io.micronaut.core.annotation.Nullable;
 import io.micronaut.http.HttpRequest;
-import io.micronaut.security.authentication.AuthenticationProvider;
 import io.micronaut.security.authentication.AuthenticationRequest;
 import io.micronaut.security.authentication.AuthenticationResponse;
+import io.micronaut.security.authentication.provider.HttpRequestReactiveAuthenticationProvider;
 import jakarta.inject.Singleton;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 
 @Singleton // <1>
-public class AuthenticationProviderUserPassword implements AuthenticationProvider<HttpRequest<?>> { // <2>
+public class AuthenticationProviderUserPassword<B> implements HttpRequestReactiveAuthenticationProvider<B> { // <2>
 
     @Override
-    public Publisher<AuthenticationResponse> authenticate(@Nullable HttpRequest<?> httpRequest,
-                                                          AuthenticationRequest<?, ?> authenticationRequest) {
+    public Publisher<AuthenticationResponse> authenticate(
+            @Nullable HttpRequest<B> httpRequest,
+            @NonNull AuthenticationRequest<String, String> authenticationRequest
+    ) {
         return Flux.create(emitter -> {
             if (authenticationRequest.getIdentity().equals("sherlock") &&
                     authenticationRequest.getSecret().equals("password")) {
