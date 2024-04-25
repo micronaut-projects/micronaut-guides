1131abb909e0af132c89c29010a52bf929159949
Tim Yates <tim.yates@gmail.com>
2024-04-25 16:51:35 +0100

    JsonPath should be a test dependency, not compile

--- buildSrc/src/main/java/io/micronaut/guides/feature/JsonPath.java
+++ buildSrc/src/main/java/io/micronaut/guides/feature/JsonPath.java
@@ -2,11 +2,13 @@ package io.micronaut.guides.feature;
 
 import jakarta.inject.Singleton;
 
+import static io.micronaut.starter.build.dependencies.Scope.TEST;
+
 @Singleton
 public class JsonPath extends AbstractFeature {
 
     public JsonPath() {
-        super("json-path");
+        super("json-path", TEST);
     }
 }
 
