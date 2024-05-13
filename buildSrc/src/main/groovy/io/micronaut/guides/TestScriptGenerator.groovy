0f228e40bb58394e0c98548962c3940b7e0af295
Tim Yates <tim.yates@gmail.com>
2024-05-13 10:32:39 +0100

    Fix guide generation when tests skipped (#1464)
    In https://github.com/micronaut-projects/micronaut-guides/pull/1459 we added the ability to totally skip a build tool/language.
    
    This broke guides which are generated but their tests are skipped (as they require resource we don't have on CI).
    
    Fixes #1462

--- buildSrc/src/main/groovy/io/micronaut/guides/TestScriptGenerator.groovy
+++ buildSrc/src/main/groovy/io/micronaut/guides/TestScriptGenerator.groovy
@@ -142,6 +142,9 @@ kill_kotlin_daemon () {
                 String folder = GuideProjectGenerator.folderName(metadata.slug, guidesOption)
                 BuildTool buildTool = folder.contains(MAVEN.toString()) ? MAVEN : GRADLE
                 if (metadata.apps.any { it.name == DEFAULT_APP_NAME } ) {
+                    if (metadata.shouldSkip(buildTool)) {
+                        continue
+                    }
                     def defaultApp = metadata.apps.find { it.name == DEFAULT_APP_NAME }
                     if (!nativeTest || supportsNativeTest(defaultApp, guidesOption)) {
                         def features = defaultApp.getFeatures(guidesOption.language)
