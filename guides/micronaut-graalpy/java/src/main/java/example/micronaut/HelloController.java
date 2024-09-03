bd6476cd3724cc1ecc14a4fe6808762d0b461406
Sergio del Amo <sergio.delamo@softamo.com>
2024-09-03 18:31:08 +0200

    guide improvements

--- guides/micronaut-graalpy/java/src/main/java/example/micronaut/HelloController.java
+++ guides/micronaut-graalpy/java/src/main/java/example/micronaut/HelloController.java
@@ -1,24 +1,37 @@
+/*
+ * Copyright 2017-2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package example.micronaut;
 
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
-import jakarta.inject.Inject;
 
 @Controller("/hello") // <1>
-public class HelloController {
+class HelloController {
 
-    private HelloModule hello; // <2>
+    private final HelloModule hello; // <2>
 
-    @Inject // <3>
-    public void inject(HelloModule hello) {
+    HelloController(HelloModule hello) {
         this.hello = hello;
     }
 
     @Get // <4>
     @Produces(MediaType.TEXT_PLAIN) // <5>
-    public String index() {
+    String index() {
         return hello.hello("World"); // <6>
     }
 }
