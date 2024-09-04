f73aaf582d363ec2dd298da22668381ae7ffff1f
Sergio del Amo <sergio.delamo@softamo.com>
2024-09-04 08:19:01 +0200

    add license

--- guides/micronaut-graalpy-python-package/java/src/main/java/example/micronaut/PygalController.java
+++ guides/micronaut-graalpy-python-package/java/src/main/java/example/micronaut/PygalController.java
@@ -1,21 +1,36 @@
+/*
+ * Copyright 2017-2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package example.micronaut;
 
-import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
-import jakarta.inject.Inject;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
 
 @Controller("/pygal") // <1>
-public class PygalController {
+class PygalController {
 
-    private PygalModule pygal; // <2>
+    private final PygalModule pygal; // <2>
 
-    @Inject // <3>
-    public void inject(PygalModule pygal) {
+    PygalController(PygalModule pygal) {
         this.pygal = pygal;
     }
 
+    @ExecuteOn(TaskExecutors.BLOCKING)
     @Get // <4>
     @Produces("image/svg+xml") // <5>
     public String index() {
--- guides/micronaut-graalpy-python-package/java/src/test/java/example/micronaut/PygalControllerTest.java
+++ guides/micronaut-graalpy-python-package/java/src/test/java/example/micronaut/PygalControllerTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package example.micronaut;
 
 import io.micronaut.http.HttpRequest;
@@ -11,21 +26,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @MicronautTest // <1>
-public class PygalControllerTest {
-
-    @Inject
-    EmbeddedServer server;
-
-    @Inject
-    @Client("/")  // <2>
-    HttpClient client;
+class PygalControllerTest {
 
     @Test
-    void testPygalResponse() {
+    void testPygalResponse(@Client("/") HttpClient client) { // <2>
         String response = client.toBlocking().retrieve(HttpRequest.GET("/pygal")); // <3>
         assertTrue(response.contains("<svg xmlns:xlink"));
         assertTrue(response.contains("<title>Pygal</title>"));
         assertTrue(response.contains("<g class=\"graph stackedbar-graph vertical\">"));
     }
-
 }
