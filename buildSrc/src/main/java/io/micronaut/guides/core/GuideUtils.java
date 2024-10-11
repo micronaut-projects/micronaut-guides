35f5fbecaa656144eb7925b893a851983fd82d62
Sergio del Amo <sergio.delamo@softamo.com>
2024-10-11 12:06:31 +0200

    merge guide

---
+++ buildSrc/src/main/java/io/micronaut/guides/core/GuideUtils.java
@@ -0,0 +1,13 @@
+package io.micronaut.guides.core;
+
+public final class GuideUtils {
+
+    private GuideUtils() {
+
+    }
+
+    public static Guide merge(Guide base, Guide guide) {
+        //TODO
+        return guide;
+    }
+}
