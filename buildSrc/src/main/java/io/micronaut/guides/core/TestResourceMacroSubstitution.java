1f2a8d7d930e25cbbda5d00b2ddbc9d47567e4eb
Andrea La Grotteria <andrea.la.grotteria@oracle.com>
2024-10-28 14:17:10 +0100

    Simplify testResource macro.

--- buildSrc/src/main/java/io/micronaut/guides/core/TestResourceMacroSubstitution.java
+++ buildSrc/src/main/java/io/micronaut/guides/core/TestResourceMacroSubstitution.java
@@ -1,43 +1,27 @@
 package io.micronaut.guides.core;
 
-import io.micronaut.core.annotation.NonNull;
+import io.micronaut.guides.core.asciidoc.Classpath;
 import jakarta.inject.Singleton;
 
-import java.util.List;
-
-import static io.micronaut.guides.core.MacroUtils.*;
-import static io.micronaut.guides.core.MacroUtils.addIncludes;
-
 @Singleton
-public class TestResourceMacroSubstitution implements MacroSubstitution {
-    private final GuidesConfiguration guidesConfiguration;
-    private final LicenseLoader licenseLoader;
+public class TestResourceMacroSubstitution extends SourceBlockMacroSubstitution {
+    private static final String MACRO_TESTRESOURCE = "testResource";
     public TestResourceMacroSubstitution(GuidesConfiguration guidesConfiguration, LicenseLoader licenseLoader) {
-        this.guidesConfiguration = guidesConfiguration;
-        this.licenseLoader = licenseLoader;
+        super(licenseLoader, guidesConfiguration);
     }
 
     @Override
-    public String substitute(String str, String slug, GuidesOption option) {
-        for(String line : findMacroLines(str, "testResource")){
-
-            String name = extractName(line, "testResource");
-            String appName = extractAppName(line);
-            List<String> tags = extractTags(line);
-            String indent = extractIndent(line);
-            String sourcePath = testResourcePath(appName, name);
-            String extension = resolveAsciidoctorLanguage(name);
-
-            List<String> lines = addIncludes(option, licenseLoader, guidesConfiguration, slug, sourcePath, extension, indent, tags);
-
-            str = str.replace(line,String.join("\n", lines));
-        }
-        return str;
+    public String getMacroName() {
+        return MACRO_TESTRESOURCE;
     }
 
-    @NonNull
-    private static String testResourcePath(@NonNull String appName, @NonNull String fileName) {
-        String module = appName.isEmpty() ? "" : appName + "/";
-        return module + "src/" + "test/resources/" + fileName;
+    @Override
+    public Classpath getClasspath() {
+        return Classpath.TEST;
+    }
+
+    @Override
+    public FileType getFileType() {
+        return FileType.RESOURCE;
     }
 }
