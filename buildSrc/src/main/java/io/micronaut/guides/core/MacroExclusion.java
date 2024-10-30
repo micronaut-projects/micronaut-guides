3e83cb376d2a0e861121a98a6a35bbd36b3e6568
Sergio del Amo <sergio.delamo@softamo.com>
2024-10-30 09:34:30 +0100

    extract constant

--- buildSrc/src/main/java/io/micronaut/guides/core/MacroExclusion.java
+++ buildSrc/src/main/java/io/micronaut/guides/core/MacroExclusion.java
@@ -3,6 +3,7 @@ package io.micronaut.guides.core;
 import java.util.List;
 
 abstract class MacroExclusion implements MacroSubstitution {
+    private static final String LINE_BREAK = "\n";
 
     protected abstract String getMacroName();
 
@@ -13,9 +14,9 @@ abstract class MacroExclusion implements MacroSubstitution {
         for (List<String> group : MacroUtils.findMacroGroupsNested(str, getMacroName())) {
             List<String> params = MacroUtils.extractMacroGroupParameters(group.get(0), getMacroName());
             if (shouldExclude(params, option, guide)) {
-                str = str.replace(String.join("\n", group)+"\n", "");
+                str = str.replace(String.join(LINE_BREAK, group)+ LINE_BREAK, "");
             } else{
-                str = str.replace(String.join("\n", group), String.join("\n", group.subList(1, group.size()-1)));
+                str = str.replace(String.join(LINE_BREAK, group), String.join(LINE_BREAK, group.subList(1, group.size()-1)));
             }
         }
         return str;
