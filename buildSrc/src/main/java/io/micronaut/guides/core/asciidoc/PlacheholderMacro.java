129fc42dbc6e014ec4a63d0e62038328c85e412c
Sergio del Amo <sergio.delamo@softamo.com>
2024-10-31 12:46:31 +0100

    add spaces

--- buildSrc/src/main/java/io/micronaut/guides/core/asciidoc/PlacheholderMacro.java
+++ buildSrc/src/main/java/io/micronaut/guides/core/asciidoc/PlacheholderMacro.java
@@ -9,9 +9,9 @@ public record PlacheholderMacro(String name, String target) {
 
     public static Optional<PlacheholderMacro> of(String name, String str) {
         if (str.startsWith(MACRO_BRACKET) && str.endsWith(name + MACRO_BRACKET)) {
-            String target = str.replace(name + MACRO_BRACKET,"")
-                    .replace(MACRO_BRACKET,"")
-                    .replace(MACRO_NAME_SEPARATOR,"");
+            String target = str.replace(name + MACRO_BRACKET, "")
+                    .replace(MACRO_BRACKET, "")
+                    .replace(MACRO_NAME_SEPARATOR, "");
             return Optional.of(new PlacheholderMacro(name, target));
         }
         return Optional.empty();
