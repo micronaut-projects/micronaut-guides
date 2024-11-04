78757e3112e3c6af31381e1eb89a7b3a1ece9dab
Sergio del Amo <sergio.delamo@softamo.com>
2024-11-04 15:32:17 +0100

    use concurrent hashMap

--- buildSrc/src/main/java/io/micronaut/guides/core/DefaultLicenseLoader.java
+++ buildSrc/src/main/java/io/micronaut/guides/core/DefaultLicenseLoader.java
@@ -10,39 +10,34 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.time.LocalDate;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Singleton
 public class DefaultLicenseLoader implements LicenseLoader {
     private static final Logger LOG = LoggerFactory.getLogger(DefaultLicenseLoader.class);
     private final int numberOfLines;
     private final String licenseHeaderText;
+    private final Map<Integer, String> headerByYear = new ConcurrentHashMap<>();
 
     public DefaultLicenseLoader(GuidesConfiguration guidesConfiguration,
                                 ResourceLoader resourceLoader) {
         Optional<InputStream> resourceAsStreamOptional = resourceLoader.getResourceAsStream(guidesConfiguration.getLicensePath());
-        this.licenseHeaderText = resourceAsStreamOptional.map(DefaultLicenseLoader::readLicenseHeader).orElse("");
+        this.licenseHeaderText = resourceAsStreamOptional.map(this::readLicenseHeader).orElse("");
         this.numberOfLines = (int) licenseHeaderText.lines().count() + 1;
     }
 
-    private static int countLines(InputStream inputStream) {
-        int numberOfLines = 0;
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-            numberOfLines = (int) reader.lines().count() + 1;
-        } catch (IOException e) {
-            LOG.error("", e);
-        }
-        return numberOfLines;
-    }
-
-    private static String readLicenseHeader(InputStream inputStream) {
-        StringBuilder licenseHeaderText = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-            reader.lines().forEach(line -> licenseHeaderText.append(line).append("\n"));
-        } catch (IOException e) {
-            LOG.error("", e);
-        }
-        return licenseHeaderText.toString().replace("$YEAR", String.valueOf(LocalDate.now().getYear()));
+    private String readLicenseHeader(InputStream inputStream) {
+        return headerByYear.computeIfAbsent(LocalDate.now().getYear(), year -> {
+            StringBuilder sb = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+                reader.lines().forEach(line -> sb.append(line).append("\n"));
+            } catch (IOException e) {
+                LOG.error("", e);
+            }
+            return sb.toString().replace("$YEAR", String.valueOf(year));
+        });
     }
 
     @Override
