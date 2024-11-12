1af444b39b2f9bd57fa10cad98532cbcc5997acc
Andrea La Grotteria <andrea.la.grotteria@oracle.com>
2024-11-12 16:19:38 +0100

    Implements changes.

--- buildSrc/src/main/java/io/micronaut/guides/core/DefaultGuideProjectZipper.java
+++ buildSrc/src/main/java/io/micronaut/guides/core/DefaultGuideProjectZipper.java
@@ -1,51 +1,55 @@
 package io.micronaut.guides.core;
 
 import jakarta.inject.Singleton;
+import org.apache.commons.compress.archivers.zip.UnixStat;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
+import org.apache.commons.compress.utils.IOUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 @Singleton
 public class DefaultGuideProjectZipper implements GuideProjectZipper {
     private static final List<String> EXCLUDED_FILES = List.of(".idea", ".DS_Store");
+    private static final List<String> EXECUTABLES = List.of("gradlew", "gradlew.bat", "mvnw", "mvnw.bat");
 
-    private static void zipDirectoryContents(File folder, String baseName, ZipOutputStream zipOutputStream) throws IOException {
-        File[] files = folder.listFiles();
-        if (files == null) return;
-
-        for (File file : files) {
+    private static void compressDirectoryToZipfile(String rootDir, String sourceDir, ZipArchiveOutputStream out) throws IOException {
+        for (File file : new File(sourceDir).listFiles()) {
             if (EXCLUDED_FILES.contains(file.getName())) {
                 continue;
             }
             if (file.isDirectory()) {
-                zipDirectoryContents(file, baseName + "/" + file.getName(), zipOutputStream);
+                compressDirectoryToZipfile(rootDir, sourceDir + File.separatorChar + file.getName(), out);
             } else {
-                try (FileInputStream fis = new FileInputStream(file)) {
-                    String zipEntryName = baseName + "/" + file.getName();
-                    zipOutputStream.putNextEntry(new ZipEntry(zipEntryName));
-
-                    byte[] buffer = new byte[1024];
-                    int length;
-                    while ((length = fis.read(buffer)) > 0) {
-                        zipOutputStream.write(buffer, 0, length);
-                    }
-                    zipOutputStream.closeEntry();
+                String zipPath = sourceDir.replace(rootDir, "") + '/' + file.getName();
+                if (zipPath.charAt(0) == '/') {
+                    zipPath = zipPath.substring(1);
                 }
+                ZipArchiveEntry entry = new ZipArchiveEntry(zipPath);
+                if (EXECUTABLES.contains(file.getName())) {
+                    entry.setUnixMode(UnixStat.FILE_FLAG | 0755);
+                }
+                out.putArchiveEntry(entry);
+                Path p = Paths.get(sourceDir, file.getName());
+                FileInputStream in = new FileInputStream(p.toFile());
+                IOUtils.copy(in, out);
+                IOUtils.closeQuietly(in);
+                out.closeArchiveEntry();
             }
         }
     }
 
     @Override
-    public void zipDirectory(File folderToZip, File zipFile) throws IOException {
-        try (FileOutputStream fileOutputStream = new FileOutputStream(zipFile);
-             ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream)) {
-            zipDirectoryContents(folderToZip, folderToZip.getName(), zipOutputStream);
-        }
+    public void zipDirectory(String sourceDir, String outputFile) throws IOException {
+        ZipArchiveOutputStream zipOutputStream = new ZipArchiveOutputStream(new FileOutputStream(outputFile));
+        compressDirectoryToZipfile(sourceDir, sourceDir, zipOutputStream);
+        IOUtils.closeQuietly(zipOutputStream);
     }
 
 }
--- buildSrc/src/main/java/io/micronaut/guides/core/GuideProjectZipper.java
+++ buildSrc/src/main/java/io/micronaut/guides/core/GuideProjectZipper.java
@@ -3,10 +3,9 @@ package io.micronaut.guides.core;
 import io.micronaut.core.annotation.NonNull;
 import jakarta.validation.constraints.NotNull;
 
-import java.io.File;
 import java.io.IOException;
 
 public interface GuideProjectZipper {
 
-    void zipDirectory(@NonNull @NotNull File folderToZip, @NonNull @NotNull File zipFile) throws IOException;
+    void zipDirectory(@NonNull @NotNull String sourceDir, @NonNull @NotNull String outputFile) throws IOException;
 }
