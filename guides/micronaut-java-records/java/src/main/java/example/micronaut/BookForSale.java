b3bdccd1f10fe4a5e9ee5c53551887f9926c684f
Sergio del Amo <sergio.delamo@softamo.com>
2025-04-03 15:12:59 +0200

    don’t add @ReflectiveAccess as the guide no longer uses jackson-databind

--- guides/micronaut-java-records/java/src/main/java/example/micronaut/BookForSale.java
+++ guides/micronaut-java-records/java/src/main/java/example/micronaut/BookForSale.java
@@ -16,14 +16,12 @@
 package example.micronaut;
 
 import io.micronaut.core.annotation.NonNull;
-import io.micronaut.core.annotation.ReflectiveAccess;
 import io.micronaut.serde.annotation.Serdeable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Serdeable // <1>
-@ReflectiveAccess // <2>
-public record BookForSale(@NonNull @NotBlank String isbn,  // <3>
+public record BookForSale(@NonNull @NotBlank String isbn,  // <2>
                           @NonNull @NotBlank String title,
                           @NonNull @NotNull BigDecimal price) { }
