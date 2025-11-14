9eaa8dfa86db29d93bb24319207f9a4a83ec037d
Sergio del Amo <sergio.delamo@softamo.com>
2025-11-14 18:49:40 +0100

    Micronaut Data JDBC Many to Many guide (#1610)

---
+++ guides/micronaut-data-many-to-many-base/java/src/main/java/example/micronaut/UserRoleId.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2017-2025 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.micronaut;
+
+import io.micronaut.data.annotation.Embeddable;
+import io.micronaut.data.annotation.Relation;
+
+import java.util.Objects;
+
+@Embeddable // <1>
+public class UserRoleId {
+
+    @Relation(value = Relation.Kind.MANY_TO_ONE) // <2>
+    private final UserEntity user;
+
+    @Relation(value = Relation.Kind.MANY_TO_ONE) // <2>
+    private final Role role;
+
+    public UserRoleId(UserEntity user, Role role) {
+        this.user = user;
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserRoleId userRoleId = (UserRoleId) o;
+        return role.id().equals(userRoleId.getRole().id()) &&
+                user.id().equals(userRoleId.getUser().id());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(role.id(), user.id());
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+}
