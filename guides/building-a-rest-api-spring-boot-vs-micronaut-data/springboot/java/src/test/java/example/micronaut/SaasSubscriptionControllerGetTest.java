62ed515f050d61a568396e488879854c1876ce42
Sergio del Amo <sergio.delamo@softamo.com>
2026-04-13 13:02:27 +0200

    fix guide

--- guides/building-a-rest-api-spring-boot-vs-micronaut-data/springboot/java/src/test/java/example/micronaut/SaasSubscriptionControllerGetTest.java
+++ guides/building-a-rest-api-spring-boot-vs-micronaut-data/springboot/java/src/test/java/example/micronaut/SaasSubscriptionControllerGetTest.java
@@ -19,14 +19,16 @@ import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.TestRestTemplate;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // <1>
+@AutoConfigureTestRestTemplate
 class SaasSubscriptionControllerGetTest {
 
     @Autowired // <2>
