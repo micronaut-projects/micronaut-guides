7b2181a7c82efed1957cc11787c5718b57a51a8b
Sergio del Amo <sergio.delamo@softamo.com>
2026-05-26 12:42:47 +0200

    Add Groovy scope types guide (#1644)
    * Add Groovy scope types guide
    
    * remove languages

---
+++ guides/micronaut-scope-types/groovy/src/main/groovy/example/micronaut/singleton/RobotController.groovy
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2017-2026 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.micronaut.singleton
+/*
+//tag::pkg[]
+package example.micronaut.singleton
+//end::pkg[]
+*/
+
+//tag::imports[]
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+//end::imports[]
+
+//tag::controller[]
+@Controller('/singleton') // <1>
+//end::controller[]
+//tag::clazz[]
+class RobotController {
+
+    private final RobotFather father
+    private final RobotMother mother
+
+    RobotController(RobotFather father,  // <2>
+                    RobotMother mother) { // <3>
+        this.father = father
+        this.mother = mother
+    }
+
+    @Get // <4>
+    List<String> children() {
+        [
+                father.child().serialNumber,
+                mother.child().serialNumber
+        ]
+    }
+}
+//end::clazz[]
