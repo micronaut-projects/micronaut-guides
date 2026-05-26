615bd6f1ec957b99b4a60e4f0d56d0833cabcaa2
Sergio del Amo <sergio.delamo@softamo.com>
2026-05-26 06:46:53 +0200

    Add Groovy AWS Lambda request stream handler guide
    Co-Authored-By: Paperclip <noreply@paperclip.ing>

---
+++ guides/micronaut-serverless-function-aws-lambda-request-stream-handler-base/groovy/src/main/groovy/example/micronaut/RequestFunction.groovy
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2017-2026 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.micronaut
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
+import io.micronaut.function.FunctionBean
+import io.micronaut.json.JsonMapper
+import jakarta.inject.Inject
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import java.util.function.Function
+
+@FunctionBean('requestfunction') // <1>
+class RequestFunction implements Function<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> { // <2>
+
+    private static final Logger LOG = LoggerFactory.getLogger(RequestFunction)
+
+    @Inject // <3>
+    JsonMapper jsonMapper
+
+    @Override
+    APIGatewayProxyResponseEvent apply(APIGatewayProxyRequestEvent requestEvent) {
+        LOG.info('request {}', requestEvent)
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+        try {
+            String json = new String(jsonMapper.writeValueAsBytes([message: 'Hello World']))
+            response.statusCode = 200
+            response.body = json
+        } catch (IOException e) {
+            response.statusCode = 500
+        }
+        LOG.info('response {}', response)
+        response
+    }
+}
---
+++ guides/micronaut-serverless-function-aws-lambda-request-stream-handler-base/groovy/src/test/groovy/example/micronaut/FunctionRequestHandlerSpec.groovy
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2017-2026 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.micronaut
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
+import io.micronaut.json.JsonMapper
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+
+class FunctionRequestHandlerSpec extends Specification {
+
+    @AutoCleanup // <2>
+    @Shared
+    FunctionRequestHandler handler = new FunctionRequestHandler() // <1>
+
+    void 'test handler'() {
+        given:
+        JsonMapper jsonMapper = handler.applicationContext.getBean(JsonMapper)
+        ByteArrayOutputStream baos = new ByteArrayOutputStream()
+        InputStream inputStream = createInputStreamRequest(jsonMapper)
+
+        when:
+        handler.execute(inputStream, baos) // <3>
+
+        then:
+        baos.toString() == '{"statusCode":200,"body":"{\\"message\\":\\"Hello World\\"}"}'
+
+        cleanup:
+        inputStream.close()
+        baos.close()
+    }
+
+    private static InputStream createInputStreamRequest(JsonMapper jsonMapper) throws IOException {
+        new ByteArrayInputStream(jsonMapper.writeValueAsBytes(createRequest()))
+    }
+
+    private static APIGatewayProxyRequestEvent createRequest() {
+        APIGatewayProxyRequestEvent request = new APIGatewayProxyRequestEvent()
+        request.httpMethod = 'GET'
+        request.path = '/'
+        request
+    }
+}
