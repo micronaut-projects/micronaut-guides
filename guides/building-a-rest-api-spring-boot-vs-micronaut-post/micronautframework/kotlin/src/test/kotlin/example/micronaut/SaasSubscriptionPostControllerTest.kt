6dcb1e17e47068f4089bca7447560f40112be4a4
Sergio del Amo <sergio.delamo@softamo.com>
2026-05-26 15:07:19 +0200

    Address Kotlin POST guide review comments

--- guides/building-a-rest-api-spring-boot-vs-micronaut-post/micronautframework/kotlin/src/test/kotlin/example/micronaut/SaasSubscriptionPostControllerTest.kt
+++ guides/building-a-rest-api-spring-boot-vs-micronaut-post/micronautframework/kotlin/src/test/kotlin/example/micronaut/SaasSubscriptionPostControllerTest.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2026 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package example.micronaut
 
 import com.jayway.jsonpath.JsonPath
@@ -6,11 +21,13 @@ import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.annotation.Sql
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.net.URI
 
+@Sql(value = ["classpath:schema.sql", "classpath:data.sql"])
 @MicronautTest // <1>
 class SaasSubscriptionPostControllerTest {
 
