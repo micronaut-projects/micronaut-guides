2de5453d7bcd1d241fc181737d32f18719ad31da
Sergio del Amo <sergio.delamo@softamo.com>
2026-05-27 06:47:00 +0200

    Use coordinate lookup for Jakarta Persistence API

--- buildSrc/src/main/java/io/micronaut/guides/feature/JakartaPersistenceApi.java
+++ buildSrc/src/main/java/io/micronaut/guides/feature/JakartaPersistenceApi.java
@@ -1,21 +1,31 @@
 package io.micronaut.guides.feature;
 
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.starter.application.ApplicationType;
 import io.micronaut.starter.application.generator.GeneratorContext;
 import io.micronaut.starter.build.dependencies.Dependency;
+import io.micronaut.starter.feature.Feature;
 import jakarta.inject.Singleton;
 
 @Singleton
-public class JakartaPersistenceApi extends AbstractFeature {
-    public JakartaPersistenceApi() {
-        super("jakarta-persistence-api", "jakarta.persistence-api");
+public class JakartaPersistenceApi implements Feature {
+    private static final String NAME = "jakarta-persistence-api";
+    private static final String ARTIFACT_ID = "jakarta.persistence-api";
+
+    @Override
+    public @NonNull String getName() {
+        return NAME;
+    }
+
+    @Override
+    public boolean supports(ApplicationType applicationType) {
+        return true;
     }
 
     @Override
     public void apply(GeneratorContext generatorContext) {
         generatorContext.addDependency(Dependency.builder()
-                .groupId("jakarta.persistence")
-                .artifactId("jakarta.persistence-api")
-                .version("3.2.0")
+                .lookupArtifactId(ARTIFACT_ID)
                 .compileOnly()
                 .build());
     }
